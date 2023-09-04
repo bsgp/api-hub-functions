4b6cc5d0de9a817255f249398489b0b60767f749
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-04 09:38:05 +0900

    feat: migrate files to prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems

--- prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems/Function/3.js
+++ prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems/Function/3.js
@@ -115,7 +115,7 @@ module.exports = async (draft, { request, odata }) => {
   function getDate(startDate) {
     const dateStr = startDate.replace(/^\/Date\(/, "").replace(")/", "");
     const date = Date(dateStr);
-    //const year = date.getFullYear();
-    return date;
+    const year = date.getFullYear();
+    return year;
   }
 };
