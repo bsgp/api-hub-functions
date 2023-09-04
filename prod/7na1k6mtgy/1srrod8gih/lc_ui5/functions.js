a6a428a0d185b58cf8c19c41a252a44d7e70cc6e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-04 14:19:10 +0900

    feat: migrate files to prod/7na1k6mtgy/1srrod8gih/lc_ui5

---
+++ prod/7na1k6mtgy/1srrod8gih/lc_ui5/functions.js
@@ -0,0 +1,37 @@
+module.exports.getMetaById = async (id, context) => {
+  const { dynamodb, tableName, binaryAttributes, unzip } = context;
+
+  const result = await dynamodb.getItem(
+    tableName,
+    { pkid: "meta", skid: id },
+    { useCustomerRole: false }
+  );
+
+  let paths;
+  if (result.paths && result.paths.length > 0) {
+    paths = await dynamodb.batchGetItem(
+      tableName,
+      result.paths.map((path) => ({ pkid: "path", skid: path })),
+      { useCustomerRole: false }
+    );
+  }
+
+  if (result === undefined) {
+    const newError = new Error("No metadata found");
+    newError.errorCode = "NO_META";
+    throw newError;
+  }
+
+  return {
+    ...result,
+    id,
+    paths,
+    ...binaryAttributes.reduce((acc, key) => {
+      if (result[key] !== undefined) {
+        acc[key] = JSON.parse(unzip(result[key]));
+      }
+
+      return acc;
+    }, {}),
+  };
+};
