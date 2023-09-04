a76141a5ee5339f7ded4b8a81b1c2c2b50228920
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-04 09:43:56 +0900

    feat: migrate files to dev/3dbhzxqle8/dpznzimld2/aaaa

--- dev/3dbhzxqle8/dpznzimld2/aaaa/Function/3.js
+++ dev/3dbhzxqle8/dpznzimld2/aaaa/Function/3.js
@@ -1,3 +1,6 @@
-module.exports = async () => {
-  // function@!11
-}
+module.exports = async (draft) => {
+  const response = {
+    message: "test Message",
+  };
+  draft.response.body = response;
+};
