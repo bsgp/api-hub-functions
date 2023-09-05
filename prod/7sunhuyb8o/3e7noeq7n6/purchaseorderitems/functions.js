53401027eef4427208fc41577f340c110ab020ba
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-05 10:28:08 +0900

    feat: migrate files to prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems

--- prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/functions.js
+++ prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/functions.js
@@ -38,3 +38,105 @@ const convDate = (dayjs, dateStr, format = "YYYY-MM-DD") => {
 };
 
 module.exports.convDate = convDate;
+
+const getPurchaseOrdersFilterParams = (params = {}, dayjs) => {
+  if (Object.entries(params).length === 0 && params.constructor === Object) {
+    throw new Error("검색 조건이 없습니다.");
+  }
+
+  const _filter = [
+    params.confirmIndicatior === "true" ? `POCONFIRM_KUT eq '102'` : ``,
+    params.confirmIndicatior === "false" ? `POCONFIRM_KUT eq '101'` : ``,
+    (() => {
+      const ds = parseInt(params.deliveryStatus);
+      if (params.deliveryStatus) {
+        if (ds === 1 || ds === 2) {
+          return [
+            `((PurchaseOrderDeliveryStatusCode eq '1')`,
+            "or",
+            `(PurchaseOrderDeliveryStatusCode eq '2'))`,
+          ].join(" ");
+        } else {
+          return `PurchaseOrderDeliveryStatusCode eq '${ds}'`;
+        }
+      } else {
+        return "";
+      }
+    })(),
+    // TODO: 공급사 조회 FILTER 추가(20.05.19)
+    params.supplierID
+      ? `(SellerParty/PartyID eq '${params.supplierID.toUpperCase()}')`
+      : "",
+    params.confirmDateFrom && params.confirmDateTo
+      ? `(CONFIRMDATE_KUT ge datetimeoffset'${convDate(
+          dayjs,
+          params.confirmDateFrom,
+          "YYYY-MM-DDTHH:mm:ss.SSS"
+        )}Z' and CONFIRMDATE_KUT le datetimeoffset'${convDate(
+          dayjs,
+          params.confirmDateTo,
+          "YYYY-MM-DDT23:59:59"
+        )}Z')`
+      : ``,
+    params.orderDateFrom && params.orderDateTo
+      ? `(CreationDate ge datetimeoffset'${convDate(
+          dayjs,
+          params.orderDateFrom,
+          "YYYY-MM-DDTHH:mm:ss.SSS"
+        )}Z' and CreationDate le datetimeoffset'${convDate(
+          dayjs,
+          params.orderDateTo,
+          "YYYY-MM-DDT23:59:59"
+        )}Z')`
+      : ``,
+    [
+      "(PurchaseOrderLifeCycleStatusCode eq '6'",
+      "PurchaseOrderLifeCycleStatusCode eq '7'",
+      "PurchaseOrderLifeCycleStatusCode eq '9'",
+      "PurchaseOrderLifeCycleStatusCode eq '10')",
+    ].join(" or "),
+  ];
+
+  if (params.purchaseOrderID) {
+    let purchaseOrderID = "";
+    if (params.purchaseOrderID.length <= 10) {
+      if (params.purchaseOrderID.constructor === Array) {
+        purchaseOrderID = params.purchaseOrderID
+          .map((id) => {
+            return `ID eq '${id}'`;
+          })
+          .join(" or ");
+      } else purchaseOrderID = `ID eq '${params.purchaseOrderID}'`;
+    }
+    if (purchaseOrderID) {
+      _filter.push(`(${purchaseOrderID})`);
+    }
+  }
+  return _filter.filter(Boolean);
+};
+
+module.exports.getPurchaseOrderParams = (params, dayjs) => {
+  const filter = getPurchaseOrdersFilterParams(params, dayjs);
+  return {
+    "sap-language": "ko",
+    $orderby: "ID desc",
+    $top: "500",
+    $format: "json",
+    $inlinecount: "allpages",
+    $filter: filter.join(" and "),
+    $expand: [
+      "BillToParty/BillToPartyDisplayName",
+      "BillToParty/BillToPartyPostalAddress",
+      "SellerParty/SellerPartyDisplayName",
+      "SellerParty/SellerPartyPostalAddress",
+      "EmployeeResponsibleParty/EmployeeResponsiblePartyDisplayName",
+      "Item",
+      "Item/PurchaseOrderItemScheduleLine",
+      "Item/PurchaseOrderReceivingItemSite",
+      "Item/PurchaseOrderItemAttachmentFolder",
+      "Item/PurchaseOrderShipToItemLocation/AddressSnapshotPostalAddress",
+      "Item/PurchaseOrderItemText",
+      "PurchaseOrderText",
+    ],
+  };
+};
