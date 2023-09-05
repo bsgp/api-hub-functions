f0b4dbbe10906cb4c2506460b7875e123632ce36
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-05 10:26:58 +0900

    feat: migrate files to prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems

---
+++ prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/functions.js
@@ -0,0 +1,40 @@
+module.exports.fetchAll = async (odata, { url, username, password }) => {
+  // url에 skip이 들어가 있으면 안됨, $top이 있어도 전체 data를 받아옴
+  const getData = (skip = 0) =>
+    odata.get({ url: `${url}&$skip=${skip}`, username, password });
+  try {
+    const fetchData = await getData();
+    if (Number(fetchData.d.__count) === fetchData.d.results.length) {
+      return { result: fetchData.d.results };
+    } else {
+      const data = fetchData.d.results;
+      while (data.length < Number(fetchData.d.__count)) {
+        const reFetchData = await getData(data.length);
+        data.push(...reFetchData.d.results);
+      }
+      return { result: data };
+    }
+  } catch (error) {
+    return { errorMsg: error.message, result: [], url };
+  }
+};
+
+const convDate = (dayjs, dateStr, format = "YYYY-MM-DD") => {
+  if (!dateStr) {
+    return "";
+  }
+  let date;
+  if (typeof dateStr === "string") {
+    if (/^\d{1,}$/.test(dateStr)) {
+      date = dateStr;
+    } else {
+      const numberString = dateStr.replace(/^\/Date\(/, "").replace(")/", "");
+      if (/^\d{1,}$/.test(numberString)) {
+        date = new Date(parseInt(numberString, 10));
+      } else date = numberString;
+    }
+  }
+  return dayjs(date).add(9, "hour").format(format);
+};
+
+module.exports.convDate = convDate;
