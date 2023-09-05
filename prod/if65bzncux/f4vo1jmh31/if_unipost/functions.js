ac0b3448a970ae740e7396f895ddc3d8a916292c
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-05 10:54:02 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_unipost

--- prod/if65bzncux/f4vo1jmh31/if_unipost/functions.js
+++ prod/if65bzncux/f4vo1jmh31/if_unipost/functions.js
@@ -11,11 +11,57 @@ module.exports.getSecretKey = async ({ restApi, tryit }) => {
   const secretKey = tryit(() => result.body.response.secretKey);
   if (secretKey) {
     return secretKey;
-  } else {
-    const err = new Error("Failed to get secret key from unipost");
-    err.description = tryit(
-      () => "JSON.stringify(result);\n" + JSON.stringify(result)
-    );
-    throw err;
   }
+
+  const err = new Error("Failed to get secret key from unipost");
+
+  err.description = tryit(
+    () => "JSON.stringify(result);\n" + JSON.stringify(result)
+  );
+
+  throw err;
+};
+
+const getToken = async (secretKey, usId, { restApi, tryit }) => {
+  const result = await restApi.post({
+    url: [
+      "https://contdev.unipost.co.kr/unicloud/cont/api/getContUserToken",
+    ].join("?"),
+    headers: {
+      "content-type": "application/json;charset=UTF-8",
+      secretKey,
+    },
+    body: {
+      usId: "bsg_cont_work01",
+    },
+  });
+
+  const token = tryit(() => result.body.response.token);
+  if (token) {
+    return token;
+  }
+
+  const err = new Error("Failed to get token from unipost");
+
+  err.description = tryit(
+    () => "JSON.stringify(result);\n" + JSON.stringify(result)
+  );
+
+  throw err;
+};
+
+module.exports.getTokenForWork = async (secretKey, { restApi, tryit }) => {
+  const result = await getToken(secretKey, "bsg_cont_work01", {
+    restApi,
+    tryit,
+  });
+  return result;
+};
+
+module.exports.getTokenForRead = async (secretKey, { restApi, tryit }) => {
+  const result = await getToken(secretKey, "bsg_cont_read01", {
+    restApi,
+    tryit,
+  });
+  return result;
 };
