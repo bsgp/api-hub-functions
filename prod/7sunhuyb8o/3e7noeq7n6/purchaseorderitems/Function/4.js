8cc2ab919fa359bbb050a9711b4bcd61c11e0eec
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-05 10:23:41 +0900

    feat: migrate files to prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems

--- prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/Function/4.js
+++ prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/Function/4.js
@@ -4,6 +4,6 @@ module.exports = async (
 ) => {
   // 	draft.json.username = env.BYD_ID;
   // draft.json.password = env.BYD_PASSWORD;
-  // draft.json.rootURL = [`${env.BYD_URL}`, "/sap/byd/odata/cust/v1"].join("");
-  draft.response.body = { ...draft.response.body };
+  const url = [draft.json.rootURL].join("");
+  draft.response.body = { ...draft.response.body, url };
 };
