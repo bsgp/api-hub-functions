0447752be0b292b66aa96ea1745293bd68b8d11d
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-06 09:43:29 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -8,15 +8,17 @@ module.exports = async (draft, { sql }) => {
       table.charset("utf8mb4");
       table.string("id").notNullable();
       table.string("text").defaultTo("");
-      table.boolean("deleted").notNullable().defaultTo(false);
-      table.datetime("created_at", { precision: 6 }).defaultTo(mysql.fn.now(6));
-      table.datetime("updated_at", { precision: 6 }).defaultTo(mysql.fn.now(6));
-      table.string("created_by").defaultTo("");
-      table.string("updated_by").defaultTo("");
+
       table.primary(["id"]);
     })
     .run();
 
   draft.response.body[spec.name] =
     result.statusCode === 200 ? "Succeed" : result.body;
+
+  // table.boolean("deleted").notNullable().defaultTo(false);
+  // table.datetime("created_at", { precision: 6 }).defaultTo(mysql.fn.now(6));
+  // table.datetime("updated_at", { precision: 6 }).defaultTo(mysql.fn.now(6));
+  // table.string("created_by").defaultTo("");
+  // table.string("updated_by").defaultTo("");
 };
