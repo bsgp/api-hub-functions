29e9a581dca6f25d2def04914fe1ad860935716e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-06 11:33:40 +0900

    feat: migrate files to prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems

--- prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/Function/4.js
+++ prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/Function/4.js
@@ -1,15 +1,31 @@
-module.exports = async (draft, { fn, dayjs }) => {
-  const params = draft.json.params;
-  // 	draft.json.username = env.BYD_ID;
-  // draft.json.password = env.BYD_PASSWORD;
+module.exports = async (draft, { fn, dayjs, odata, user }) => {
+  const { params, username, password } = draft.json.params;
+
   const queryStringObj = fn.getPO_ItemParams(params, dayjs);
   const queryString = Object.keys(queryStringObj)
     .map((key) => `${key}=${queryStringObj[key]}`)
     .join("&");
-  const url = [
+  const po_url = [
     draft.json.rootURL,
     "/bsg_purchaseorder/ItemCollection?",
     queryString,
   ].join("");
-  draft.response.body = { ...draft.response.body, params, url };
+
+  const queryPO = await fn
+    .fetchAll(odata, { url: po_url, username, password })
+    .then(({ result = [] }) => result);
+  const queryPurchaseOrders = queryPO.filter((po) => {
+    const userID = `${user.id}`;
+    return (
+      !params.isSupplier ||
+      (params.isSupplier && po.SellerParty.PartyID === userID.toUpperCase())
+    );
+  });
+
+  draft.response.body = {
+    ...draft.response.body,
+    params,
+    url: po_url,
+    queryPurchaseOrders,
+  };
 };
