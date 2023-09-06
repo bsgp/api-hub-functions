976895b3859e3ed7c15babe9a97d83975ffbe674
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-06 11:38:12 +0900

    feat: migrate files to prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems

--- prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/Function/4.js
+++ prod/7sunhuyb8o/3e7noeq7n6/purchaseorderitems/Function/4.js
@@ -1,31 +1,39 @@
 module.exports = async (draft, { fn, dayjs, odata, user }) => {
   const { params, username, password } = draft.json.params;
+  try {
+    const queryStringObj = fn.getPO_ItemParams(params, dayjs);
+    const queryString = Object.keys(queryStringObj)
+      .map((key) => `${key}=${queryStringObj[key]}`)
+      .join("&");
+    const po_url = [
+      draft.json.rootURL,
+      "/bsg_purchaseorder/ItemCollection?",
+      queryString,
+    ].join("");
 
-  const queryStringObj = fn.getPO_ItemParams(params, dayjs);
-  const queryString = Object.keys(queryStringObj)
-    .map((key) => `${key}=${queryStringObj[key]}`)
-    .join("&");
-  const po_url = [
-    draft.json.rootURL,
-    "/bsg_purchaseorder/ItemCollection?",
-    queryString,
-  ].join("");
+    const queryPO_Item = await fn
+      .fetchAll(odata, { url: po_url, username, password })
+      .then(({ result = [] }) => result);
+    const queryPurchaseOrderItems = queryPO_Item.filter((po) => {
+      const userID = `${user.id}`;
+      return (
+        !params.isSupplier ||
+        (params.isSupplier && po.PO.SellerPartyID === userID.toUpperCase())
+      );
+    });
 
-  const queryPO = await fn
-    .fetchAll(odata, { url: po_url, username, password })
-    .then(({ result = [] }) => result);
-  const queryPurchaseOrders = queryPO.filter((po) => {
-    const userID = `${user.id}`;
-    return (
-      !params.isSupplier ||
-      (params.isSupplier && po.SellerParty.PartyID === userID.toUpperCase())
-    );
-  });
-
-  draft.response.body = {
-    ...draft.response.body,
-    params,
-    url: po_url,
-    queryPurchaseOrders,
-  };
+    draft.response.body = {
+      ...draft.response.body,
+      params,
+      url: po_url,
+      queryPurchaseOrderItems,
+    };
+  } catch (error) {
+    draft.response.body = {
+      ...draft.response.body,
+      params,
+      E_STATUS: "F",
+      E_MESSAGE: error.message,
+    };
+  }
 };
