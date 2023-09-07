51b3d7db5629ce23c91b793fe28b3983a7b419c6
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-07 10:05:07 +0900

    feat: migrate files to prod/7na1k6mtgy/wk79tmdlvu/delete_develop_roadmap

--- prod/7na1k6mtgy/wk79tmdlvu/delete_develop_roadmap/Function/4.js
+++ prod/7na1k6mtgy/wk79tmdlvu/delete_develop_roadmap/Function/4.js
@@ -67,11 +67,72 @@ module.exports = async (draft, context) => {
     return;
   }
 
-  console.log("historyResult", historyResult);
+  //삭제가 적용된 데이터 가져오기
+  const queryLOT = builder
+    .select(commentsName, [
+      `${commentsHistoryName}.*`,
+      `${commentsFilesName}.*`,
+      `${commentsName}.*`,
+      `${commentsHistoryName}.created_at AS history_created_at`,
+      `${commentsHistoryName}.name AS history_name`,
+    ])
+    .leftJoin(
+      commentsFilesName,
+      `${commentsName}.comment_id`,
+      "=",
+      `${commentsFilesName}.comment_id`
+    )
+    .leftJoin(
+      commentsHistoryName,
+      `${commentsName}.comment_id`,
+      "=",
+      `${commentsHistoryName}.comment_id`
+    )
+    .where({
+      [`${commentsName}.comment_id`]: data.comment_id,
+    })
+    .orderBy([{ column: "history_created_at", order: "desc" }]);
+
+  console.log("sql", queryLOT.toSQL().sql);
+  console.log("bindings", queryLOT.toSQL().bindings);
+  const resultLOT = await queryLOT.run();
+  console.log("select result list", resultLOT.body.list);
+  // const histories = await builder
+  //   .select(commentsHistoryName)
+  //   .where("comment_id", data.comment_id)
+  //   .run();
+  // console.log("생성된 히스토리들", histories.body.list);
+
+  // 데이터 하나로 묶는 작업
+  const newComment = resultLOT.body.list.reduce((acc, data) => {
+    if (!data.history_id) {
+      return acc;
+    }
+    const historyData = {
+      history_id: data.history_id,
+      history_name: data.history_name,
+      new_value: data.new_value,
+      old_value: data.old_value,
+      history_created_at: data.history_created_at,
+    };
+    if (!acc.history_id) {
+      return {
+        ...data,
+        history: [historyData],
+      };
+    } else {
+      return {
+        ...acc,
+        history: acc.history.concat(historyData),
+      };
+    }
+  }, {});
+
+  console.log("final result", newComment);
   draft.response.body = {
     E_STATUS: "S",
     E_MESSAGE: `data patch success`,
-    history: newCommentHistory,
+    result: newComment,
   };
 
   return;
