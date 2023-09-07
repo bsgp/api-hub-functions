e7a4ee7dd27730bf7578fbaae24a944a6347019a
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-07 11:33:48 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/functions.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/functions.js
@@ -1,37 +1,133 @@
-module.exports.change = (mysql) => (table) => {
+/**
+ */
+module.exports.change =
+  ({ mysql }) =>
+  (table) => {
+    table.charset("utf8mb4");
+
+    table.string("type", 10).notNullable();
+    table.string("row_key", 20).notNullable();
+    table.string("id", 5).notNullable();
+
+    table.string("changed_by", 30).defaultTo("");
+    table.datetime("changed_by", { precision: 6 }).defaultTo(mysql.fn.now(6));
+    table.json("content");
+
+    table.primary(["type", "row_key", "id"]);
+  };
+
+/**
+ */
+module.exports.contract = () => (table) => {
   table.charset("utf8mb4");
 
-  table.string("type", 10).notNullable();
-  table.string("row_key", 20).notNullable();
-  table.string("id", 5).notNullable();
+  table.string("id", 10).notNullable();
 
-  table.string("changed_by", 30).defaultTo("");
-  table.datetime("changed_by", { precision: 6 }).defaultTo(mysql.fn.now(6));
-  table.json("content");
+  table.string("prod_date", 8).defaultTo("");
+  table.string("bukrs", 4).defaultTo("");
+  table.string("name", 200).defaultTo("");
+  table.string("type", 1).defaultTo("");
+  table.string("start_date", 8).defaultTo("");
+  table.string("end_date", 8).defaultTo("");
+  table.boolean("renewal_ind", 30);
+  table.string("renewal_period", 3).defaultTo("");
+  table.string("curr_key", 5).defaultTo("");
+  table.decimal("dmbtr", 23, 2).defaultTo(0);
+  table.decimal("dmbtr_local", 23, 2).defaultTo(0);
+  table.string("dmbtr_local", 5).defaultTo("");
+  table.string("status", 3).defaultTo("");
 
-  table.primary(["type", "row_key", "id"]);
+  table.primary(["id"]);
 };
 
-module.exports.contract = (table) => {
-  table.charset("utf8mb4");
-};
+/**
+ */
+module.exports.ref_doc =
+  ({ makeid }) =>
+  (table) => {
+    table.charset("utf8mb4");
 
-module.exports.ref_doc = (table) => {
-  table.charset("utf8mb4");
-};
+    table.string("contract_id", 10).notNullable();
+    table.string("id", 5).defaultTo(makeid(5));
 
-module.exports.cost_object = (table) => {
-  table.charset("utf8mb4");
-};
+    table.string("type", 5).defaultTo("");
+    table.string("item_id", 5).defaultTo("");
+    table.string("doc_id", 36).defaultTo("");
 
-module.exports.bill = (table) => {
-  table.charset("utf8mb4");
-};
+    table.primary(["contract_id", "id"]);
+  };
 
-module.exports.party = (table) => {
-  table.charset("utf8mb4");
-};
+/**
+ */
+module.exports.cost_object =
+  ({ makeid }) =>
+  (table) => {
+    table.charset("utf8mb4");
 
-module.exports.letter_appr = (table) => {
-  table.charset("utf8mb4");
-};
+    table.string("contract_id", 10).notNullable();
+    table.string("id", 5).defaultTo(makeid(5));
+
+    table.string("type", 3).defaultTo("");
+    table.string("cost_object_id", 36).defaultTo("");
+    table.string("name", 100).defaultTo("");
+    table.string("cost_type", 3).defaultTo("");
+    table.decimal("dmbtr", 23, 2).defaultTo(0);
+    table.decimal("dmbtr_local", 23, 2).defaultTo(0);
+    table.string("start_date", 8).defaultTo("");
+    table.string("end_date", 8).defaultTo("");
+
+    table.primary(["contract_id", "id"]);
+  };
+
+/**
+ */
+module.exports.bill =
+  ({ makeid }) =>
+  (table) => {
+    table.charset("utf8mb4");
+
+    table.string("contract_id", 10).notNullable();
+    table.string("id", 5).defaultTo(makeid(5));
+
+    table.string("cost_object_id", 36).defaultTo("");
+    table.string("reason_text", 200).defaultTo("");
+    table.decimal("dmbtr", 23, 2).defaultTo(0);
+    table.decimal("dmbtr_local", 23, 2).defaultTo(0);
+    table.string("curr_key", 5).defaultTo("");
+    table.string("curr_local", 5).defaultTo("");
+
+    table.primary(["contract_id", "id"]);
+  };
+
+/**
+ */
+module.exports.party =
+  ({ makeid }) =>
+  (table) => {
+    table.charset("utf8mb4");
+    //
+    table.string("contract_id", 10).notNullable();
+    table.string("id", 5).defaultTo(makeid(5));
+    //
+    table.string("stems10_cn", 1).defaultTo("");
+    table.string("stems10_ko", 1).defaultTo("");
+    table.string("name", 100).defaultTo("");
+    table.string("id", 36).defaultTo("");
+    table.string("type", 5).defaultTo("");
+    //
+    table.primary(["contract_id", "id"]);
+  };
+
+/**
+ */
+module.exports.letter_appr =
+  ({ makeid }) =>
+  (table) => {
+    table.charset("utf8mb4");
+    //
+    table.string("contract_id", 10).notNullable();
+    table.string("id", 5).defaultTo(makeid(5));
+    //
+    //
+    table.primary(["contract_id", "id"]);
+  };
