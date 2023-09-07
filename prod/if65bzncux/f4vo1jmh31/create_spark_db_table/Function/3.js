6b053514973f4b689f7354d5809d7af73b2dc74a
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-07 10:43:12 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
@@ -8,7 +8,7 @@ module.exports = async (draft, { file }) => {
   const lastestTableConfig = (configFile && JSON.parse(configFile)) || {};
   /**
    * db 업데이트 시
-   * table name의 number++ && function#4 schema update
+   * function#3 table name의 number++ && function#4 schema update
    */
   const tables = {
     change: {
@@ -54,6 +54,6 @@ module.exports = async (draft, { file }) => {
     }
     return acc;
   }, {});
-  draft.json.tables = tables;
+  draft.json.changed = changed;
   draft.response.body = { tableConfig: newTableConfig, changed };
 };
