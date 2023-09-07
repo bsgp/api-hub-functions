5db34e67adcc704f23306f36b065f3f661324180
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-07 11:39:02 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
@@ -12,31 +12,31 @@ module.exports = async (draft, { file }) => {
    */
   const tables = {
     change: {
-      name: "change_999",
+      name: "change_0",
       desc: "Spark DB change history table",
     },
     contract: {
-      name: "contract_999",
+      name: "contract_0",
       desc: "Contract info DB table",
     },
     ref_doc: {
-      name: "ref_doc_999",
+      name: "ref_doc_0",
       desc: "Reference document(MM,FI) info DB table",
     },
     cost_object: {
-      name: "cost_object_999",
+      name: "cost_object_0",
       desc: "WBS, CostCenter info DB table",
     },
     bill: {
-      name: "bill_999",
+      name: "bill_0",
       desc: "bill reson text DB",
     },
     party: {
-      name: "party_999",
+      name: "party_0",
       desc: "Party(supplier, customer) info DB",
     },
     letter_appr: {
-      name: "letter_appr_999",
+      name: "letter_appr_0",
       desc: "groupware letter approval info DB",
     },
   };
