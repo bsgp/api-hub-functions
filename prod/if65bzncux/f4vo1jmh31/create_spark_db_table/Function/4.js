5300ad58fd38379ac60a1bb2ef624c3188072cb6
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-07 11:41:50 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -18,6 +18,7 @@ module.exports = async (draft, { fn, sql, makeid }) => {
         .run();
       draft.response.body[spec.name] =
         result.statusCode === 200 ? "Succeed" : result.body;
+      return true;
     })
   );
 };
