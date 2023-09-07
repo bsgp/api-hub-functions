774f35e7dd547b05b6952b1f43dbb8e4f1f03bf5
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-07 10:57:54 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/functions.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/functions.js
@@ -1,3 +1,27 @@
-module.exports.change = () => {
-  // your function code
+module.exports.change = (table) => {
+  table.charset("utf8mb4");
+};
+
+module.exports.contract = (table) => {
+  table.charset("utf8mb4");
+};
+
+module.exports.ref_doc = (table) => {
+  table.charset("utf8mb4");
+};
+
+module.exports.cost_object = (table) => {
+  table.charset("utf8mb4");
+};
+
+module.exports.bill = (table) => {
+  table.charset("utf8mb4");
+};
+
+module.exports.party = (table) => {
+  table.charset("utf8mb4");
+};
+
+module.exports.letter_appr = (table) => {
+  table.charset("utf8mb4");
 };
