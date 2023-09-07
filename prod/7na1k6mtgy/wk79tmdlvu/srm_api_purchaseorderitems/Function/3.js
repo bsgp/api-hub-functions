ae6e65e87021e7b21bff45af732b0b4d8de6353e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-07 09:35:37 +0900

    feat: migrate files to prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems

--- prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems/Function/3.js
+++ prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems/Function/3.js
@@ -177,15 +177,15 @@ module.exports = async (draft, { request, odata }) => {
         (acc, curr) => {
           const idnObj = curr.InboundDelivery;
           const cCode = idnObj.CancellationStatusCode;
-          const rCode = idnObj.ReleaseStatusCode;
+          //const rCode = idnObj.ReleaseStatusCode;
           const dPCode = idnObj.DeliveryProcessingStatusCode;
           const qtyObj = curr.Item.DeliveryQuantity;
           if (cCode === "1") {
-            if (rCode === "3" && dPCode === "1") {
-              acc.sum = Number(qtyObj.Quantity) + acc.sum;
-            }
+            // if (rCode === "3" && dPCode === "1") {
+            //   acc.sum = Number(qtyObj.Quantity) + acc.sum;
+            // }
             //if (rCode === "1" && dPCode === "1") {
-            if (rCode === "1") {
+            if (dPCode === "1") {
               acc.sum = Number(qtyObj.Quantity) + acc.sum;
             }
             //acc.sum = Number(qtyObj.Quantity) + acc.sum;
