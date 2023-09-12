91b7a0d14226b9a3054fa47621b64bb58ddb9ef3
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-12 16:33:24 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/4.js
@@ -2,7 +2,8 @@ module.exports = async (draft, { lib, request }) => {
   const { clone } = lib;
   draft.json.newData = clone(request.body.Data);
   switch (request.body.InterfaceId) {
-    case "IF-CT-101":
+    case "IF-CT-101": {
+      // GET
       draft.response.body = {
         request_contractID: draft.json.newData.contractID,
         contract: {
@@ -16,6 +17,39 @@ module.exports = async (draft, { lib, request }) => {
         E_MESSAGE: "TEST",
       };
       break;
+    }
+    case "IF-CT-102": {
+      // POST
+      draft.response.body = {
+        request_contractID: draft.json.newData.contractID,
+        contract: {
+          type: "P",
+          partyList: [],
+          costObjectList: [],
+          billList: [],
+          attachmentList: [],
+        },
+        E_STATUS: "S",
+        E_MESSAGE: "TEST",
+      };
+      break;
+    }
+    case "IF-CT-103": {
+      // PATCH
+      draft.response.body = {
+        request_contractID: draft.json.newData.contractID,
+        contract: {
+          type: "P",
+          partyList: [],
+          costObjectList: [],
+          billList: [],
+          attachmentList: [],
+        },
+        E_STATUS: "S",
+        E_MESSAGE: "TEST",
+      };
+      break;
+    }
     default:
       //
       break;
