3dbad540b71ea018dc4aa388b3b84336f5c6379a
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-14 09:47:16 +0900

    feat: migrate files to prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems

--- prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems/Function/3.js
+++ prod/7na1k6mtgy/wk79tmdlvu/srm_api_purchaseorderitems/Function/3.js
@@ -91,7 +91,7 @@ module.exports = async (draft, { request, odata }) => {
 
       return {
         index: idx + 1,
-        isScheduled: item.PurchaseOrderItemScheduleLine.StartDateTime,
+        isScheduled: item.PurchaseOrderItemScheduleLine.__metadata.uri,
         confirmIndicatior: item.PO.SRM001_KUT,
         supplyStatusText: item.PurchaseOrderDeliveryStatusCodeText,
         materialID: item.ProductID,
