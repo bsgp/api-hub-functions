01be241f493a66d2724e25db76486e2037fa419a
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-14 16:18:18 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
@@ -2,6 +2,7 @@ module.exports = async (draft) => {
   draft.response.body = {
     request_contractID: draft.json.newData.contractID,
     contract: {
+      contractID: "7546",
       type: "P",
       partyList: [],
       costObjectList: [],
