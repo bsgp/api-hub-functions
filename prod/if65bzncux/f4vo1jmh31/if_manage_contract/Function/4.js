72fb4f8db9a70aac335fcb6d09f6d1b77fddbc93
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-14 09:51:18 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/4.js
@@ -2,56 +2,16 @@ module.exports = async (draft, { lib, request }) => {
   const { clone } = lib;
   draft.json.newData = clone(request.body.Data);
   switch (request.body.InterfaceId) {
-    case "IF-CT-101": {
-      // GET
-      draft.response.body = {
-        request_contractID: draft.json.newData.contractID,
-        contract: {
-          type: "P",
-          partyList: [],
-          costObjectList: [],
-          billList: [],
-          attachmentList: [],
-        },
-        E_STATUS: "S",
-        E_MESSAGE: "TEST",
-      };
+    case "IF-CT-101": // GET
+      draft.json.nextNodeKey = "Function#5";
       break;
-    }
-    case "IF-CT-102": {
-      // POST
-      draft.response.body = {
-        request_contractID: draft.json.newData.contractID,
-        contract: {
-          type: "P",
-          partyList: [],
-          costObjectList: [],
-          billList: [],
-          attachmentList: [],
-        },
-        E_STATUS: "S",
-        E_MESSAGE: "TEST",
-      };
+    case "IF-CT-102": // POST
+      draft.json.nextNodeKey = "Function#6";
       break;
-    }
-    case "IF-CT-103": {
-      // PATCH
-      draft.response.body = {
-        request_contractID: draft.json.newData.contractID,
-        contract: {
-          type: "P",
-          partyList: [],
-          costObjectList: [],
-          billList: [],
-          attachmentList: [],
-        },
-        E_STATUS: "S",
-        E_MESSAGE: "TEST",
-      };
+    case "IF-CT-103": // PATCH
+      draft.json.nextNodeKey = "Function#7";
       break;
-    }
     default:
-      //
       break;
   }
 };
