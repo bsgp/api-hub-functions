a11cb43990ef20b0a4ad5814825383d9a1356df8
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-15 11:17:29 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
@@ -1,21 +1,25 @@
-module.exports = async (draft, { sql }) => {
+module.exports = async (draft, { sql, tryit }) => {
   const { tables, newData } = draft.json;
-  const query = sql("mysql", { useCustomRole: false })
-    .select(tables.contract.name)
-    .where("id", "like", Number(newData.contractID));
-  const queryResult = await query.run();
+  let contract = {};
+  if (newData.contractID) {
+    const query = sql("mysql", { useCustomRole: false })
+      .select(tables.contract.name)
+      .where("id", "like", Number(newData.contractID));
+    const queryResult = await query.run();
+    const contractID = tryit(() => queryResult.body.list[0].id, "");
+    if (contractID) {
+      contract = { ...contract, ...queryResult.body.list[0] };
+    }
+  }
 
   draft.response.body = {
     request_contractID: newData.contractID,
-    tables,
-    queryResult,
     contract: {
-      contractID: "7546",
-      type: "P",
       partyList: [],
       costObjectList: [],
       billList: [],
       attachmentList: [],
+      ...contract,
     },
     E_STATUS: "S",
     E_MESSAGE: "TEST",
