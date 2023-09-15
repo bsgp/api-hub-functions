bf8079377fc791e579c052ca75b085ba8b7383a7
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-15 11:25:27 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/functions.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/functions.js
@@ -11,8 +11,8 @@ module.exports.getDB_Object = (data, key, contract_id) => {
       renewal_ind: data.form.renewal_ind,
       renewal_period: data.form.renewal_period,
       curr_key: data.form.curr_key,
-      dmbtr: data.form.dmbtr.replace(/,/g, ""),
-      dmbtr_local: data.form.dmbtr_local.replace(/,/g, ""),
+      dmbtr: (data.form.dmbtr || "").replace(/,/g, ""),
+      dmbtr_local: (data.form.dmbtr_local || "").replace(/,/g, ""),
       curr_local: data.form.curr_local,
       status: data.form.status,
     };
@@ -35,8 +35,8 @@ module.exports.getDB_Object = (data, key, contract_id) => {
         cost_object_id: item.cost_object_id,
         name: item.name,
         cost_type: item.cost_type,
-        dmbtr: item.dmbtr.replace(/,/g, ""),
-        dmbtr_local: item.dmbtr_local.replace(/,/g, ""),
+        dmbtr: (item.dmbtr || "").replace(/,/g, ""),
+        dmbtr_local: (item.dmbtr_local || "").replace(/,/g, ""),
         start_date: item.start_date,
         end_date: item.end_date,
       }));
@@ -47,8 +47,8 @@ module.exports.getDB_Object = (data, key, contract_id) => {
         id: item.id,
         cost_object_id: item.cost_object_id,
         reason_text: item.reason_text,
-        dmbtr: item.dmbtr.replace(/,/g, ""),
-        dmbtr_local: item.dmbtr_local.replace(/,/g, ""),
+        dmbtr: (item.dmbtr || "").replace(/,/g, ""),
+        dmbtr_local: (item.dmbtr_local || "").replace(/,/g, ""),
         curr_key: item.curr_key,
         curr_local: item.curr_local,
       }));
