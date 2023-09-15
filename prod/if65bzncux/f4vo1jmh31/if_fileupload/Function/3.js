73ebe269b7ce0bb9c0e6d6af6fbf7bfaeeaafa53
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-15 09:05:48 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_fileupload

--- prod/if65bzncux/f4vo1jmh31/if_fileupload/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/if_fileupload/Function/3.js
@@ -1,5 +1,5 @@
 module.exports = async (draft, { request, file }) => {
-  draft.response.body = await Promise.all(
+  const uploadResult = await Promise.all(
     (request.body.Data.files || []).map(async (fileData) => {
       const { tempFilePath, fileType, path } = fileData;
       const data = await file.get(tempFilePath, {
@@ -12,4 +12,17 @@ module.exports = async (draft, { request, file }) => {
       return fileResponse;
     })
   );
+  if (uploadResult.find((res) => !res)) {
+    draft.response.body = {
+      E_STATUS: "F",
+      E_MESSAGE: "FileUpload failed",
+      uploadResult,
+    };
+  } else {
+    draft.response.body = {
+      E_STATUS: "S",
+      E_MESSAGE: "FileUpload succeed",
+      uploadResult,
+    };
+  }
 };
