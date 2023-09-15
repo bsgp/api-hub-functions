617fd317821e41e9a846b191fd9d19c44054ecac
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-15 10:32:40 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/6.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/6.js
@@ -1,13 +1,15 @@
-module.exports = async (draft, { fn }) => {
+module.exports = async (draft, { fn, sql }) => {
   const { tables, newData } = draft.json;
   const contract = fn.getDB_Object(newData, "contract");
+
+  const builder = sql("mysql", { useCustomRole: false });
+  // const contractValidator = await builder.validator(tables.contract.name);
 
   draft.response.body = {
     table: tables.contract.name,
     contract,
+    builder,
   };
-  // const builder = sql("mysql", { useCustomRole: false });
-  // const contractValidator = await builder.validator(tables.contract.name);
   // if (!contractValidator(contract).isValid) {
   //   draft.response.body = {
   //     E_STATUS: "F",
