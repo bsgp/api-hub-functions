f517cec55bf908aaaae64e80eff357df72a8736e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-15 10:31:47 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/6.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/6.js
@@ -1,53 +1,58 @@
-module.exports = async (draft, { fn, sql }) => {
+module.exports = async (draft, { fn }) => {
   const { tables, newData } = draft.json;
   const contract = fn.getDB_Object(newData, "contract");
-  const builder = sql("mysql", { useCustomRole: false });
-  const contractValidator = await builder.validator(tables.contract.name);
-  if (!contractValidator(contract).isValid) {
-    draft.response.body = {
-      E_STATUS: "F",
-      E_MESSAGE: "Valid falied",
-    };
-    return;
-  }
 
-  const createContract = await builder
-    .insert(tables.contract.name, contract)
-    .run();
-  if (createContract.statusCode === 200) {
-    draft.response.body = {
-      E_STATUS: "S",
-      E_MESSAGE: "contract insert successfully",
-      createContract,
-    };
-  } else {
-    draft.response.body = {
-      E_STATUS: "F",
-      E_MESSAGE: `Failed save ${tables.contract.name}`,
-      createContract,
-    };
-    draft.response.statusCode = 400;
-    return;
-  }
+  draft.response.body = {
+    table: tables.contract.name,
+    contract,
+  };
+  // const builder = sql("mysql", { useCustomRole: false });
+  // const contractValidator = await builder.validator(tables.contract.name);
+  // if (!contractValidator(contract).isValid) {
+  //   draft.response.body = {
+  //     E_STATUS: "F",
+  //     E_MESSAGE: "Valid falied",
+  //   };
+  //   return;
+  // }
+
+  // const createContract = await builder
+  //   .insert(tables.contract.name, contract)
+  //   .run();
+  // if (createContract.statusCode === 200) {
+  //   draft.response.body = {
+  //     E_STATUS: "S",
+  //     E_MESSAGE: "contract insert successfully",
+  //     createContract,
+  //   };
+  // } else {
+  //   draft.response.body = {
+  //     E_STATUS: "F",
+  //     E_MESSAGE: `Failed save ${tables.contract.name}`,
+  //     createContract,
+  //   };
+  //   draft.response.statusCode = 400;
+  //   return;
+  // }
 
   // const ref_doc = [];
   // const cost_object = [];
   // const bill = [];
   // const party = [];
 
-  draft.response.body = {
-    E_STATUS: "S",
-    E_MESSAGE: "TEST",
-    ...draft.response.body,
-    request_contractID: draft.json.newData.contractID,
-    tables: draft.json.tables,
-    contract: {
-      type: "P",
-      contractID: "7775577",
-      partyList: [],
-      costObjectList: [],
-      billList: [],
-      attachmentList: [],
-    },
-  };
+  // draft.response.body = {
+  //   E_STATUS: "S",
+  //   E_MESSAGE: "TEST",
+  //   ...draft.response.body,
+  //   request_contractID: draft.json.newData.contractID,
+  //   tables: draft.json.tables,
+  //   contract: {
+  //     type: "P",
+  //     contractID: "7775577",
+  //     partyList: [],
+  //     costObjectList: [],
+  //     billList: [],
+  //     attachmentList: [],
+  //   },
+  // };
 };
