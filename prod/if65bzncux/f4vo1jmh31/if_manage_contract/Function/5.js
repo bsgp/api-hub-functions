bd558ea6ef55ea1f3887988d71bee4b9fabded66
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-17 20:36:05 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
@@ -9,24 +9,24 @@ module.exports = async (draft, { sql, tryit }) => {
     const contractID = tryit(() => queryResult.body.list[0].id, "");
     if (contractID) {
       contract = { ...contract, ...queryResult.body.list[0], contractID };
-      const partyData = await sql("mysql", { useCustomRole: false })
-        .select(tables.party.name)
-        .where("contract_id", "like", newData.contractID)
-        .run();
-      const partyList = tryit(() => partyData.body.list, []);
-      contract = { ...contract, partyList };
+      const tableList = [];
+      await Promise.all(
+        tableList.map(async ({ tableKey, clientTableName }) => {
+          const queryTableData = await sql("mysql", { useCustomRole: false })
+            .select(tables[tableKey].name)
+            .where("contract_id", "like", contractID)
+            .run();
+          const tableData = tryit(() => queryTableData.body.list, []);
+          contract[clientTableName] = tableData;
+          return true;
+        })
+      );
     }
   }
 
   draft.response.body = {
     request_contractID: newData.contractID,
-    contract: {
-      partyList: [],
-      costObjectList: [],
-      billList: [],
-      attachmentList: [],
-      ...contract,
-    },
+    contract,
     E_STATUS: "S",
     E_MESSAGE: `계약번호: ${contract.contractID}\n조회가\n완료되었습니다`,
   };
