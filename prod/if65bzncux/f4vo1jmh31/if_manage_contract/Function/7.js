9cc7ac5833f09fa2c60bd6aa71a9450eda61d0d3
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-18 01:25:11 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/7.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/7.js
@@ -1,14 +1,42 @@
-module.exports = async (draft) => {
+module.exports = async (draft, { sql, tryit }) => {
+  const { tables, newData } = draft.json;
+  let results = {};
+  if (newData.contractID) {
+    const query = sql("mysql", { useCustomRole: false })
+      .select(tables.contract.name)
+      .where("id", "like", Number(newData.contractID));
+    const queryResult = await query.run();
+    const contractID = tryit(() => queryResult.body.list[0].id, "");
+    const tableList = ["party", "bill", "ref_doc", "cost_object", "attachment"];
+    if (contractID) {
+      results = { contract: queryResult.body.list[0], contractID };
+      await Promise.all(
+        tableList.map(async (tableKey) => {
+          const queryTableData = await sql("mysql", { useCustomRole: false })
+            .select(tables[tableKey].name)
+            .where("contract_id", "like", contractID)
+            .orderBy("index", "asc")
+            .run();
+          const tableData = tryit(() => queryTableData.body.list, []);
+          results[tableKey] = tableData;
+          return true;
+        })
+      );
+    }
+  }
+  const { contract, party, bill, cost_object, attachment } = results; //ref_doc
+
   draft.response.body = {
-    request_contractID: draft.json.newData.contractID,
+    request_contractID: newData.contractID,
     contract: {
-      type: "P",
-      partyList: [],
-      costObjectList: [],
-      billList: [],
-      attachmentList: [],
+      ...contract,
+      contractID: results.contractID,
+      partyList: party,
+      costObjectList: cost_object,
+      billList: bill,
+      attachmentList: attachment,
     },
     E_STATUS: "S",
-    E_MESSAGE: "TEST",
+    E_MESSAGE: `계약번호: ${results.contractID}\n조회가\n완료되었습니다`,
   };
 };
