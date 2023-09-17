7817af7ff8072e5dff432dbb7f088ea42bcb37ea
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-17 20:46:39 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/5.js
@@ -1,6 +1,6 @@
 module.exports = async (draft, { sql, tryit }) => {
   const { tables, newData } = draft.json;
-  let contract = {};
+  let results = {};
   if (newData.contractID) {
     const query = sql("mysql", { useCustomRole: false })
       .select(tables.contract.name)
@@ -9,7 +9,7 @@ module.exports = async (draft, { sql, tryit }) => {
     const contractID = tryit(() => queryResult.body.list[0].id, "");
     const tableList = ["party", "bill", "ref_doc", "cost_object", "attachment"];
     if (contractID) {
-      contract = { ...contract, ...queryResult.body.list[0], contractID };
+      results = { contract: queryResult.body.list[0], contractID };
       await Promise.all(
         tableList.map(async (tableKey) => {
           const queryTableData = await sql("mysql", { useCustomRole: false })
@@ -17,16 +17,23 @@ module.exports = async (draft, { sql, tryit }) => {
             .where("contract_id", "like", contractID)
             .run();
           const tableData = tryit(() => queryTableData.body.list, []);
-          contract[tableKey] = tableData;
+          results[tableKey] = tableData;
           return true;
         })
       );
     }
   }
+  const { contract, party, bill, cost_object, attachment } = results; //ref_doc
 
   draft.response.body = {
     request_contractID: newData.contractID,
-    contract,
+    contract: {
+      ...contract,
+      partyList: party,
+      costObjectList: cost_object,
+      billList: bill,
+      attachmentList: attachment,
+    },
     E_STATUS: "S",
     E_MESSAGE: `계약번호: ${contract.contractID}\n조회가\n완료되었습니다`,
   };
