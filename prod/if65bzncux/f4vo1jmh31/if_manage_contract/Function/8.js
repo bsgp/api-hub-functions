39cde9c22d80ce655a7c8881b7a23cdcdd95e6a9
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-19 10:20:02 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
@@ -1,4 +1,20 @@
-module.exports = async (draft, { request }) => {
-  // your script
-  draft.response.body = { request };
+module.exports = async (draft, { sql, tryit }) => {
+  const { tables, newData } = draft.json;
+  const queryBuilder = sql("mysql", { useCustomRole: false }).select(
+    tables.contract.name
+  );
+
+  if (newData.contractID) {
+    queryBuilder.where("id", "like", Number(newData.contractID));
+  }
+
+  const queryResult = await queryBuilder.run();
+  const results = tryit(() => queryResult.body.list, []);
+
+  draft.response.body = {
+    request: newData,
+    list: results,
+    E_STATUS: "S",
+    E_MESSAGE: `조회가\n완료되었습니다`,
+  };
 };
