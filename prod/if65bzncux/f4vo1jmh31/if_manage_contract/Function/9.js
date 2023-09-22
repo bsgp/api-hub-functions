6efe92c869f7f21f43b14fa70f3304c03a86f3c6
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-22 12:48:12 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/9.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/9.js
@@ -1,15 +1,19 @@
 module.exports = async (draft, { sql }) => {
   const { tables } = draft.json;
-  const tableData = {
-    type: "contract",
-    row_key: "1",
-    id: "12344",
-    changed_by: "test",
-    content: JSON.stringify({ id: 123, test: "test" }),
-  };
-  const postTableData = await sql("mysql", { useCustomRole: false })
-    .insert(tables["change"].name, tableData)
-    .run();
+  // const tableData = {
+  //   type: "contract",
+  //   row_key: "1",
+  //   id: "12344",
+  //   changed_by: "test",
+  //   content: JSON.stringify({ id: 123, test: "test" }),
+  // };
+  // const postTableData = await sql("mysql", { useCustomRole: false })
+  //   .insert(tables["change"].name, tableData)
+  //   .run();
+
+  const postTableData = await sql("mysql", { useCustomRole: false }).select(
+    tables["change"].name
+  );
 
   draft.response.body = {
     postTableData,
