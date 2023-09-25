b662e9d47c7ab02aaedd6d09f9f6abf93ecc5765
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-25 17:01:21 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
@@ -5,45 +5,45 @@ module.exports = async (draft, { sql, tryit, fn, dayjs }) => {
   );
 
   const queryParams = {};
-  // if (newData.partyID) {
-  //   queryBuilder
-  //     .select(
-  //       `${tables.contract.name}.*`,
-  //       `${tables.party.name}.contract_id`,
-  //       `${tables.party.name}.ref_id`
-  //     )
-  //     .leftJoin(
-  //       tables.party.name,
-  //       `${tables.contract.name}.id`,
-  //       "=",
-  //       `${tables.party.name}.contract_id`
-  //     );
-  //   // .where("party.ref_id", "like", newData.partyID);
-  // } else {
-  if (newData.contractDate[0] && newData.contractDate[1]) {
-    const from = fn.convDate(dayjs, newData.contractDate[0], "YYYYMMDD");
-    const to = fn.convDate(dayjs, newData.contractDate[1], "YYYYMMDD");
-    queryParams.from = from;
-    queryParams.to = to;
-    queryBuilder.whereBetween("prod_date", [from, to]);
+  if (newData.partyID) {
+    queryBuilder
+      .select(
+        `${tables.contract.name}.*`,
+        `${tables.party.name}.contract_id`,
+        `${tables.party.name}.ref_id`
+      )
+      .leftJoin(
+        tables.party.name,
+        `${tables.contract.name}.id`,
+        "=",
+        `${tables.party.name}.contract_id`
+      )
+      .where("ref_id", "like", newData.partyID);
+  } else {
+    if (newData.contractDate[0] && newData.contractDate[1]) {
+      const from = fn.convDate(dayjs, newData.contractDate[0], "YYYYMMDD");
+      const to = fn.convDate(dayjs, newData.contractDate[1], "YYYYMMDD");
+      queryParams.from = from;
+      queryParams.to = to;
+      queryBuilder.whereBetween("prod_date", [from, to]);
+    }
+    if (newData.contractType) {
+      queryParams.type = newData.contractType;
+      queryBuilder.where("type", "like", newData.contractType);
+    }
+    if (newData.contractStatus) {
+      queryParams.contractStatus = newData.contractStatus;
+      queryBuilder.where("status", "like", newData.contractStatus);
+    }
+    if (newData.contractID) {
+      queryParams.contractID = newData.contractID;
+      queryBuilder.where("id", "like", Number(newData.contractID));
+    }
+    if (newData.contractName) {
+      queryParams.contractName = newData.contractName;
+      queryBuilder.where("name", "like", `%${newData.contractName}%`);
+    }
   }
-  if (newData.contractType) {
-    queryParams.type = newData.contractType;
-    queryBuilder.where("type", "like", newData.contractType);
-  }
-  if (newData.contractStatus) {
-    queryParams.contractStatus = newData.contractStatus;
-    queryBuilder.where("status", "like", newData.contractStatus);
-  }
-  if (newData.contractID) {
-    queryParams.contractID = newData.contractID;
-    queryBuilder.where("id", "like", Number(newData.contractID));
-  }
-  if (newData.contractName) {
-    queryParams.contractName = newData.contractName;
-    queryBuilder.where("name", "like", `%${newData.contractName}%`);
-  }
-  // }
 
   const queryResult = await queryBuilder.run();
   const results = tryit(() => queryResult.body.list, []);
