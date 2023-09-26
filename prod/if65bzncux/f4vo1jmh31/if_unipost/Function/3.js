2a7ea557af2c817dbbfff2dfcc693341f95a98ff
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-26 13:19:05 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_unipost

--- prod/if65bzncux/f4vo1jmh31/if_unipost/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/if_unipost/Function/3.js
@@ -1,4 +1,4 @@
-module.exports = async (draft, { restApi, tryit, fn }) => {
+module.exports = async (draft, { restApi, fn }) => {
   // const params = new URLSearchParams();
   // params.set("bukrs", "2000");
   // params.set("date_from", "20220801");
@@ -6,15 +6,35 @@ module.exports = async (draft, { restApi, tryit, fn }) => {
   // params.set("dctyp", "BZM01");
   // const queryString = params.toString();
 
-  const secretKey = await fn.getSecretKey({ restApi, tryit });
+  const secretKey = await fn.getSecretKey({ restApi });
 
-  const token = await fn.getTokenForWork(secretKey, { restApi, tryit });
+  switch (draft.json.ifObj.InterfaceId) {
+    case "IF-CT-007":
+      {
+        const result = await fn.getTemplateList();
 
-  const result = await fn.getTemplateList();
-
-  draft.response.body = {
-    secretKey,
-    token,
-    result,
-  };
+        draft.response.body = {
+          list: result,
+        };
+      }
+      break;
+    case "IF-CT-008":
+      {
+        const token = await fn.getTokenForWork(secretKey, { restApi });
+        draft.response.body = {
+          token,
+        };
+      }
+      break;
+    case "IF-CT-009":
+      {
+        const token = await fn.getTokenForRead(secretKey, { restApi });
+        draft.response.body = {
+          token,
+        };
+      }
+      break;
+    default:
+      break;
+  }
 };
