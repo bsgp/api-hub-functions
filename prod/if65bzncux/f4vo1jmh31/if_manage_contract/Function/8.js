e6a589d1f838b686abaf1195ebc5840ad5f1e526
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-26 15:08:58 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
@@ -16,7 +16,7 @@ module.exports = async (draft, { sql, tryit, fn, dayjs }) => {
       "=",
       `${tables.party.name}.contract_id`
     )
-    .where("party_deleted", false);
+    .where("deleted", false);
 
   if (newData.partyID) {
     queryBuilder
