a1ecd7ce9312af03bb3adcce07c46e0ffe5a1bbb
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-09-26 11:22:59 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/upload_if_list

--- prod/if65bzncux/f4vo1jmh31/upload_if_list/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/upload_if_list/Function/3.js
@@ -1,52 +1,5 @@
 module.exports = async (draft, { file }) => {
   const interfaces = {
-    systems: {
-      // BZM: {
-      //   name: "비즈몰",
-      //   domains: {
-      //     dev: "http://bizmall.dev.golfzon.com",
-      //     qas: "http://bizmall.qa.golfzon.com",
-      //     prd: "http://bizmalladmin.golfzon.com",
-      //   },
-      // },
-      // GBOS: {
-      //   domains: {
-      //     dev: "qas",
-      //     qas: "https://fairway.spazon.com",
-      //     prd: "http://fairway.golfzon.local",
-      //   },
-      //   headers: {
-      //     dev: "qas",
-      //     qas: {
-      //       apikey: "sap-nsonMh8cXLN6FHw5T6XO1QCqsppvEkcn",
-      //     },
-      //     prd: {
-      //       apikey: "sap-Y2ZtdKBWIvbr6vWc6cxoppMqWdXOmoQr",
-      //     },
-      //   },
-      // },
-      // GDRBI: {
-      //   domains: {
-      //     dev: "DB:VDGZ-MSDEV01.GFZ.CORP:10024",
-      //     //VDGZ-MSDEV01.GFZ.CORP//172.20.43.13
-      //     qas: "dev",
-      //     prd: "DB:PLGZ-GDRDWDB.GFZ.CORP:1433",
-      //     //PLGZ-GDRDWDB.GFZ.CORP//192.168.20.91
-      //   },
-      //   dbNames: {
-      //     dev: "GDR_DW_DB",
-      //     qas: "dev",
-      //     prd: "GOLFZON_GDRDW_DB",
-      //   },
-      // },
-    },
-    prefixes: {
-      // "IF-FI-BZM": "BZM",
-      // "IF-SD-BZ": "BZM",
-      // "IF-SD-GBS": "GBOS",
-      // "IF-FI-PLS": "MEMBER_PLATFORM",
-      // "IF-FI-GMB": "MEMBER_PLATFORM",
-    },
     ["IF-CO-001"]: {
       Type: "RFC",
       Name: "ZCO_IF_CCTR_MASTER_ALL",
@@ -94,6 +47,18 @@ module.exports = async (draft, { file }) => {
       Name: "ZCO_IF_WBS_LIST",
       TriggeredBy: "BSGON",
       Path: "/rfc",
+    },
+    ["IF-CT-007"]: {
+      Type: "API",
+      Name: "CT_TEMPLATE_LIST",
+      TriggeredBy: "UNIPOST",
+      Path: "/unipost",
+    },
+    ["IF-CT-008"]: {
+      Type: "API",
+      Name: "CT_CONT_WRITE",
+      TriggeredBy: "UNIPOST",
+      Path: "/unipost",
     },
     ["IF-CT-011"]: {
       Type: "WEBHOOK",
