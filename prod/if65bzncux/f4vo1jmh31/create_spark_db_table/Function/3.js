9de5cb346358d466148735b36779f1172c5a737e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-10-12 16:50:38 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
@@ -1,11 +1,11 @@
 module.exports = async (draft, { file }) => {
-  // let configFile;
-  // try {
-  //   configFile = await file.get("config/tables.json", { gziped: true });
-  // } catch (err) {
-  //   draft.response.body = { configFileMessage: "new Config file created" };
-  // }
-  // const lastestTableConfig = (configFile && JSON.parse(configFile)) || {};
+  let configFile;
+  try {
+    configFile = await file.get("config/tables.json", { gziped: true });
+  } catch (err) {
+    draft.response.body = { configFileMessage: "new Config file created" };
+  }
+  const lastestTableConfig = (configFile && JSON.parse(configFile)) || {};
   /**
    * db 업데이트 시
    * Function#3 table name의 number++ && Functions: schema update
@@ -50,12 +50,12 @@ module.exports = async (draft, { file }) => {
   });
 
   const changed = Object.keys(tables).reduce((acc, curr) => {
-    // if (
-    //   !lastestTableConfig[curr] ||
-    //   tables[curr].name !== lastestTableConfig[curr].name
-    // ) {
-    acc[curr] = tables[curr];
-    // }
+    if (
+      !lastestTableConfig[curr] ||
+      tables[curr].name !== lastestTableConfig[curr].name
+    ) {
+      acc[curr] = tables[curr];
+    }
     return acc;
   }, {});
   draft.json.changed = changed;
