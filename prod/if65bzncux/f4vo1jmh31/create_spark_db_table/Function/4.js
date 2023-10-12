451a8d9412d7182b83f4079b38063e7c4a49196f
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-10-12 16:51:14 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -16,23 +16,23 @@ module.exports = async (draft, { fn, sql, makeid }) => {
       const result = await mysql.table
         .create(spec.name, fn[tableKey]({ mysql, makeid }))
         .run();
-      if (result.statusCode !== 200) {
-        if (spec.desc === "Party(supplier, customer) info DB") {
-          const alterResult = await mysql.table
-            .alter(spec.name, function (table) {
-              table.string("prdnt_name", 30).defaultTo("").comment("대표자 명");
-              table.string("id_no", 20).defaultTo("").comment("사업자등록번호");
-              table.string("biz_no", 20).defaultTo("").comment("법인등록번호");
-              table.string("land_id", 2).defaultTo("").comment("국가키");
-              table.string("address", 100).defaultTo("").comment("주소");
-              table.string("tel", 20).defaultTo("").comment("연락처");
-            })
-            .run();
-          draft.response.body[spec.name] = alterResult;
-        }
-      } else
-        draft.response.body[spec.name] =
-          result.statusCode === 200 ? "Succeed" : result.body;
+      // if (result.statusCode !== 200) {
+      //   if (spec.desc === "Party(supplier, customer) info DB") {
+      //     const alterResult = await mysql.table
+      //    .alter(spec.name, function (table) {
+      //     table.string("prdnt_name", 30).defaultTo("").comment("대표자 명");
+      //     table.string("id_no", 20).defaultTo("").comment("사업자등록번호");
+      //     table.string("biz_no", 20).defaultTo("").comment("법인등록번호");
+      //     table.string("land_id", 2).defaultTo("").comment("국가키");
+      //     table.string("address", 100).defaultTo("").comment("주소");
+      //     table.string("tel", 20).defaultTo("").comment("연락처");
+      //     })
+      //   .run();
+      //     draft.response.body[spec.name] = alterResult;
+      //   }
+      // } else
+      draft.response.body[spec.name] =
+        result.statusCode === 200 ? "Succeed" : result.body;
       return true;
     })
   );
