72e8340c46f28628b27aa6bf15b9c3a818032cb7
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-10-18 16:22:51 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/11.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/11.js
@@ -1,5 +1,19 @@
-module.exports = async (draft, { request }) => {
+module.exports = async (
+  draft,
+  {
+    request,
+    // sql, env
+  }
+) => {
   // your script
+  // await sql("mysql", {
+  //   useCustomRole: false,
+  //   stage: env.CURRENT_ALIAS,
+  // })
+  //   .update(tables[tableKey].name, { deleted: true })
+  //   .where("contract_id", "like", contractID)
+  //   .where("id", "like", before.id)
+  //   .run();
   draft.response.body = {
     request,
     E_STATUS: "F",
