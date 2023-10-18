110a368f9dc7ae6b06b49105f3b7a3ce25336fa1
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-10-18 16:41:15 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/11.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/11.js
@@ -1,21 +1,15 @@
-module.exports = async (
-  draft,
-  {
-    request,
-    // sql, env
-  }
-) => {
+module.exports = async (draft, { sql, env }) => {
   const { tables, newData } = draft.json;
-  // await sql("mysql", {
-  //   useCustomRole: false,
-  //   stage: env.CURRENT_ALIAS,
-  // })
-  //   .update(tables[tableKey].name, { deleted: true })
-  //   .where("contract_id", "like", contractID)
-  //   .where("id", "like", before.id)
-  //   .run();
+  const updateResult = await sql("mysql", {
+    useCustomRole: false,
+    stage: env.CURRENT_ALIAS,
+  })
+    .update(tables.cost_object.name, { last_send_date: newData.last_send_date })
+    .where("contract_id", "like", newData.contractID)
+    .where("id", "like", newData.id)
+    .run();
   draft.response.body = {
-    request,
+    updateResult,
     tables,
     newData,
     E_STATUS: "F",
