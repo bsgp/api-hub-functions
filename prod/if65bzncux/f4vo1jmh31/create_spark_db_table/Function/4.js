87d9007a32fa6b2e98c832403f6427ab4a30c4af
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-10-18 16:17:27 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -19,26 +19,21 @@ module.exports = async (draft, { fn, sql, env, makeid }) => {
       const result = await mysql.table
         .create(spec.name, fn[tableKey]({ mysql, makeid }))
         .run();
-      // if (result.statusCode !== 200) {
-      //   if (spec.desc === "WBS, CostCenter info DB table") {
-      //     const alterResult = await mysql.table
-      //       .alter(spec.name, function (table) {
-      //         table
-      //           .string("po_number ", 10)
-      //           .defaultTo("")
-      //           .comment("구매오더번호");
-      //         table
-      //           .string("po_item_no", 10)
-      //           .defaultTo("")
-      //           .comment("구매오더항목번호");
-      //         table.string("post_date", 8).defaultTo("").comment("청구예정일");
-      //       })
-      //       .run();
-      //     draft.response.body[spec.name] = alterResult;
-      //   }
-      // } else
-      draft.response.body[spec.name] =
-        result.statusCode === 200 ? "Succeed" : result.body;
+      if (result.statusCode !== 200) {
+        if (spec.desc === "WBS, CostCenter info DB table") {
+          const alterResult = await mysql.table
+            .alter(spec.name, function (table) {
+              table
+                .datetime("last_send_date ")
+                .defaultTo(null)
+                .comment("WBS업데이트일자");
+            })
+            .run();
+          draft.response.body[spec.name] = alterResult;
+        }
+      } else
+        draft.response.body[spec.name] =
+          result.statusCode === 200 ? "Succeed" : result.body;
       return true;
     })
   );
