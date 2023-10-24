babeeb8dd7ab9ef973d360a07669f0e16a572d96
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-10-24 10:48:18 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -19,22 +19,22 @@ module.exports = async (draft, { fn, sql, env, makeid }) => {
       const result = await mysql.table
         .create(spec.name, fn[tableKey]({ mysql, makeid }))
         .run();
-      if (result.statusCode !== 200) {
-        if (spec.desc === "Party(supplier, customer) info DB") {
-          const alterResult = await mysql.table
-            .alter(spec.name, function (table) {
-              table.string("gl_group_id", 20).defaultTo("").comment("계정그룹");
-              table
-                .string("gl_group_text", 20)
-                .defaultTo("")
-                .comment("계정그룹텍스트");
-            })
-            .run();
-          draft.response.body[spec.name] = alterResult;
-        }
-      } else
-        draft.response.body[spec.name] =
-          result.statusCode === 200 ? "Succeed" : result.body;
+      // if (result.statusCode !== 200) {
+      //   if (spec.desc === "Party(supplier, customer) info DB") {
+      //     const alterResult = await mysql.table
+      //       .alter(spec.name, function (table) {
+      //         table.string("gl_group_id", 20).defaultTo("").comment("계정그룹");
+      //         table
+      //           .string("gl_group_text", 20)
+      //           .defaultTo("")
+      //           .comment("계정그룹텍스트");
+      //       })
+      //       .run();
+      //     draft.response.body[spec.name] = alterResult;
+      //   }
+      // } else
+      draft.response.body[spec.name] =
+        result.statusCode === 200 ? "Succeed" : result.body;
       return true;
     })
   );
