9203af25697a047df749db1a3d683e31e230c9b4
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-10-31 15:18:28 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -19,22 +19,20 @@ module.exports = async (draft, { fn, sql, env, makeid }) => {
       const result = await mysql.table
         .create(spec.name, fn[tableKey]({ mysql, makeid }))
         .run();
-      // if (result.statusCode !== 200) {
-      //   if (spec.desc === "MM,FI cost object DB table") {
-      //     const alterResult = await mysql.table
-      //       .alter(spec.name, function (table) {
-      //         table.string("parent_item_id", 5).defaultTo("");
-      //         table
-      //           .string("billing_index", 5)
-      //           .defaultTo("")
-      //           .comment("전표청구항목"); // 전표청구항목
-      //       })
-      //       .run();
-      //     draft.response.body[spec.name] = alterResult;
-      //   }
-      // } else
-      draft.response.body[spec.name] =
-        result.statusCode === 200 ? "Succeed" : result.body;
+      if (result.statusCode !== 200) {
+        if (spec.desc === "Actual FI cost object DB table") {
+          const alterResult = await mysql.table
+            .alter(spec.name, function (table) {
+              table.string("parent_id", 5).defaultTo("");
+              table.string("gl_account_id", 18).defaultTo("");
+              table.string("gl_account", 100).defaultTo("");
+            })
+            .run();
+          draft.response.body[spec.name] = alterResult;
+        }
+      } else
+        draft.response.body[spec.name] =
+          result.statusCode === 200 ? "Succeed" : result.body;
       return true;
     })
   );
