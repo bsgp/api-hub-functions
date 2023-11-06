950140792d85acc0b240b2a98734bc2f746c829f
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-06 10:22:30 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
@@ -14,51 +14,76 @@ module.exports = async (draft, { file, env }) => {
    * Function#3 table name의 number++ && Functions: schema update
    */
   const tables = {
-    change: {
-      name: "change_7",
-      desc: "Spark DB change history table",
-    },
-    contract: {
-      name: "contract_9",
-      desc: "Contract info DB table",
-    },
+    change: { name: "change_6", desc: "Spark DB change history table" },
+    contract: { name: "contract_8", desc: "Contract info DB table" },
     changed_contract: {
       name: "changed_contract_1",
       desc: "Changed Contract seq info DB table",
     },
     ref_doc: {
-      name: "ref_doc_10",
+      name: "ref_doc_9",
       desc: "Reference document(MM,FI) info DB table",
     },
-    cost_object: {
-      name: "cost_object_11",
-      desc: "MM,FI cost object DB table",
-    },
-    wbs: {
-      name: "wbs_1",
-      desc: "WBS DB table",
-    },
-    bill: {
-      name: "bill_12",
-      desc: "bill reson text DB",
-    },
+    cost_object: { name: "cost_object_10", desc: "MM,FI cost object DB table" },
+    wbs: { name: "wbs_0", desc: "WBS DB table" },
+    bill: { name: "bill_11", desc: "bill reson text DB" },
     actual_billing: {
       name: "actual_billing_1",
       desc: "Actual FI cost object DB table",
     },
-    party: {
-      name: "party_11",
-      desc: "Party(supplier, customer) info DB",
-    },
-    attachment: {
-      name: "attachment_9",
-      desc: "Attachment info DB",
-    },
+    party: { name: "party_10", desc: "Party(supplier, customer) info DB" },
+    attachment: { name: "attachment_8", desc: "Attachment info DB" },
     letter_appr: {
-      name: "letter_appr_6",
+      name: "letter_appr_5",
       desc: "groupware letter approval info DB",
     },
   };
+  // const tables = {
+  //   change: {
+  //     name: "change_7",
+  //     desc: "Spark DB change history table",
+  //   },
+  //   contract: {
+  //     name: "contract_9",
+  //     desc: "Contract info DB table",
+  //   },
+  //   changed_contract: {
+  //     name: "changed_contract_1",
+  //     desc: "Changed Contract seq info DB table",
+  //   },
+  //   ref_doc: {
+  //     name: "ref_doc_10",
+  //     desc: "Reference document(MM,FI) info DB table",
+  //   },
+  //   cost_object: {
+  //     name: "cost_object_11",
+  //     desc: "MM,FI cost object DB table",
+  //   },
+  //   wbs: {
+  //     name: "wbs_1",
+  //     desc: "WBS DB table",
+  //   },
+  //   bill: {
+  //     name: "bill_12",
+  //     desc: "bill reson text DB",
+  //   },
+  //   actual_billing: {
+  //     name: "actual_billing_1",
+  //     desc: "Actual FI cost object DB table",
+  //   },
+  //   party: {
+  //     name: "party_11",
+  //     desc: "Party(supplier, customer) info DB",
+  //   },
+  //   attachment: {
+  //     name: "attachment_9",
+  //     desc: "Attachment info DB",
+  //   },
+  //   letter_appr: {
+  //     name: "letter_appr_6",
+  //     desc: "groupware letter approval info DB",
+  //   },
+  // };
   draft.json.changed = tables;
   draft.response.body = { changed: tables, lastestTableConfig };
 
