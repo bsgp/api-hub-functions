3ce1b599cf77722a03de0980fc795424e3ef4d8a
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-06 10:24:10 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/3.js
@@ -13,84 +13,85 @@ module.exports = async (draft, { file, env }) => {
    * db 업데이트 시
    * Function#3 table name의 number++ && Functions: schema update
    */
-  const tables = {
-    change: { name: "change_6", desc: "Spark DB change history table" },
-    contract: { name: "contract_8", desc: "Contract info DB table" },
-    changed_contract: {
-      name: "changed_contract_1",
-      desc: "Changed Contract seq info DB table",
-    },
-    ref_doc: {
-      name: "ref_doc_9",
-      desc: "Reference document(MM,FI) info DB table",
-    },
-    cost_object: { name: "cost_object_10", desc: "MM,FI cost object DB table" },
-    wbs: { name: "wbs_0", desc: "WBS DB table" },
-    bill: { name: "bill_11", desc: "bill reson text DB" },
-    actual_billing: {
-      name: "actual_billing_1",
-      desc: "Actual FI cost object DB table",
-    },
-    party: { name: "party_10", desc: "Party(supplier, customer) info DB" },
-    attachment: { name: "attachment_8", desc: "Attachment info DB" },
-    letter_appr: {
-      name: "letter_appr_5",
-      desc: "groupware letter approval info DB",
-    },
-  };
+
   // const tables = {
-  //   change: {
-  //     name: "change_7",
-  //     desc: "Spark DB change history table",
-  //   },
-  //   contract: {
-  //     name: "contract_9",
-  //     desc: "Contract info DB table",
-  //   },
+  //   change: { name: "change_6", desc: "Spark DB change history table" },
+  //   contract: { name: "contract_8", desc: "Contract info DB table" },
   //   changed_contract: {
   //     name: "changed_contract_1",
   //     desc: "Changed Contract seq info DB table",
   //   },
   //   ref_doc: {
-  //     name: "ref_doc_10",
+  //     name: "ref_doc_9",
   //     desc: "Reference document(MM,FI) info DB table",
   //   },
-  //   cost_object: {
-  //     name: "cost_object_11",
-  //     desc: "MM,FI cost object DB table",
-  //   },
-  //   wbs: {
-  //     name: "wbs_1",
-  //     desc: "WBS DB table",
-  //   },
-  //   bill: {
-  //     name: "bill_12",
-  //     desc: "bill reson text DB",
-  //   },
+  //  cost_object:{name:"cost_object_10", desc: "MM,FI cost object DB table" },
+  //   wbs: { name: "wbs_0", desc: "WBS DB table" },
+  //   bill: { name: "bill_11", desc: "bill reson text DB" },
   //   actual_billing: {
   //     name: "actual_billing_1",
   //     desc: "Actual FI cost object DB table",
   //   },
-  //   party: {
-  //     name: "party_11",
-  //     desc: "Party(supplier, customer) info DB",
-  //   },
-  //   attachment: {
-  //     name: "attachment_9",
-  //     desc: "Attachment info DB",
-  //   },
+  //   party: { name: "party_10", desc: "Party(supplier, customer) info DB" },
+  //   attachment: { name: "attachment_8", desc: "Attachment info DB" },
   //   letter_appr: {
-  //     name: "letter_appr_6",
+  //     name: "letter_appr_5",
   //     desc: "groupware letter approval info DB",
   //   },
   // };
+  const tables = {
+    change: {
+      name: "change_7",
+      desc: "Spark DB change history table",
+    },
+    contract: {
+      name: "contract_9",
+      desc: "Contract info DB table",
+    },
+    changed_contract: {
+      name: "changed_contract_1",
+      desc: "Changed Contract seq info DB table",
+    },
+    ref_doc: {
+      name: "ref_doc_10",
+      desc: "Reference document(MM,FI) info DB table",
+    },
+    cost_object: {
+      name: "cost_object_11",
+      desc: "MM,FI cost object DB table",
+    },
+    wbs: {
+      name: "wbs_1",
+      desc: "WBS DB table",
+    },
+    bill: {
+      name: "bill_12",
+      desc: "bill reson text DB",
+    },
+    actual_billing: {
+      name: "actual_billing_1",
+      desc: "Actual FI cost object DB table",
+    },
+    party: {
+      name: "party_11",
+      desc: "Party(supplier, customer) info DB",
+    },
+    attachment: {
+      name: "attachment_9",
+      desc: "Attachment info DB",
+    },
+    letter_appr: {
+      name: "letter_appr_6",
+      desc: "groupware letter approval info DB",
+    },
+  };
   draft.json.changed = tables;
   draft.response.body = { changed: tables, lastestTableConfig };
 
-  await file.upload("config/tables.json", tables, {
-    gzip: true,
-    stage: env.CURRENT_ALIAS,
-  });
+  // await file.upload("config/tables.json", tables, {
+  //   gzip: true,
+  //   stage: env.CURRENT_ALIAS,
+  // });
 
   // const newTableConfig =
   //   await file.upload("config/tables.json", tables, {
