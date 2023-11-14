5cbf7df8c1420faa21255e7c3d709d554ca9e561
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-14 14:39:50 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_gpro

--- prod/if65bzncux/f4vo1jmh31/if_gpro/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/if_gpro/Function/3.js
@@ -7,11 +7,19 @@ module.exports = async (draft, { fn, restApi }) => {
         const token = await fn.getToken({ restApi });
         const result = fn.reverseFiDocument(token, ifObj.Data, { restApi });
 
-        draft.response.body = {
-          E_STATUS: "S",
-          E_MESSAGE: "성공",
-          ...result,
-        };
+        if (result) {
+          draft.response.body = {
+            E_STATUS: "S",
+            E_MESSAGE: "성공",
+            ...result,
+          };
+        } else {
+          draft.response.body = {
+            E_STATUS: "E",
+            E_MESSAGE: "실패",
+            RESULT: result,
+          };
+        }
       } catch (ex) {
         draft.response.body = {
           E_STATUS: "E",
