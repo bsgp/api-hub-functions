2d07dfa10ce9d4427e676e7465b2e4467319d8eb
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-16 10:25:28 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_gpro

--- prod/if65bzncux/f4vo1jmh31/if_gpro/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/if_gpro/Function/3.js
@@ -1,32 +1,13 @@
-module.exports = async (draft, { fn, request, restApi }) => {
+module.exports = async (draft) => {
   const { ifObj } = draft.json;
 
   switch (ifObj.InterfaceId) {
     case "IF-FI-004":
-      try {
-        const token = await fn.getToken({ restApi });
-        const result = await fn.reverseFiDocument(token, request.body.Data, {
-          restApi,
-        });
-
-        draft.response.body = {
-          E_STATUS: "S",
-          E_MESSAGE: "성공",
-          ...result,
-        };
-      } catch (ex) {
-        draft.response.body = {
-          E_STATUS: "E",
-          E_MESSAGE: [ex.message, ex.description].filter(Boolean).join(" "),
-        };
-      }
+      draft.json.nextNodeKey = "Function#5";
 
       break;
     default:
-      draft.response.body = {
-        E_STATUS: "E",
-        E_MESSAGE: "Can not recognize this Interface ID",
-      };
+      draft.json.nextNodeKey = "Function#6";
       break;
   }
 };
