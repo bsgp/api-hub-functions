ed3b917a30f460885c63609991f7f29e2b811066
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-16 10:29:48 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_gpro

---
+++ prod/if65bzncux/f4vo1jmh31/if_gpro/Function/6.js
@@ -0,0 +1,32 @@
+module.exports = async (draft, { fn, request, restApi }) => {
+  const { ifObj } = draft.json;
+
+  switch (ifObj.InterfaceId) {
+    case "IF-CT-002":
+      try {
+        const token = await fn.getToken({ restApi });
+        const result = await fn.postDraft(token, request.body.Data, {
+          restApi,
+        });
+
+        draft.response.body = {
+          E_STATUS: "S",
+          E_MESSAGE: "성공",
+          ...result,
+        };
+      } catch (ex) {
+        draft.response.body = {
+          E_STATUS: "E",
+          E_MESSAGE: [ex.message, ex.description].filter(Boolean).join(" "),
+        };
+      }
+
+      break;
+    default:
+      draft.response.body = {
+        E_STATUS: "E",
+        E_MESSAGE: "Can not recognize this Interface ID",
+      };
+      break;
+  }
+};
