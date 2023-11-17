b6aa94f003297122817bd07ebfe90498cad040d2
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-17 16:29:51 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_gpro

--- prod/if65bzncux/f4vo1jmh31/if_gpro/Function/6.js
+++ prod/if65bzncux/f4vo1jmh31/if_gpro/Function/6.js
@@ -38,6 +38,78 @@ module.exports = async (draft, { fn, request, restApi, flow }) => {
       }
 
       break;
+    case "IF-CO-011-BATCH":
+      try {
+        const token = await fn.getToken({ restApi });
+        const result = await fn.getOrganizationsList(token, {
+          restApi,
+        });
+
+        const rfcResult = await flow.run({
+          id: "if_rfc",
+          body: {
+            InterfaceId: "IF-CO-011",
+            Function: {
+              ...request.body.Function,
+              SysId: "G_PRO",
+              Type: "RFC",
+            },
+            Data: {
+              IT_DATA: result,
+            },
+          },
+        });
+
+        draft.response.body = {
+          E_STATUS: "S",
+          E_MESSAGE: "성공",
+          GPRO: result,
+          SAP: rfcResult,
+        };
+      } catch (ex) {
+        draft.response.body = {
+          E_STATUS: "E",
+          E_MESSAGE: [ex.message, ex.description].filter(Boolean).join(" "),
+        };
+      }
+
+      break;
+    case "IF-CO-035-BATCH":
+      try {
+        const token = await fn.getToken({ restApi });
+        const result = await fn.getOrganizationsList(token, {
+          restApi,
+        });
+
+        const rfcResult = await flow.run({
+          id: "if_rfc",
+          body: {
+            InterfaceId: "IF-CO-035",
+            Function: {
+              ...request.body.Function,
+              SysId: "G_PRO",
+              Type: "RFC",
+            },
+            Data: {
+              IT_DATA: result,
+            },
+          },
+        });
+
+        draft.response.body = {
+          E_STATUS: "S",
+          E_MESSAGE: "성공",
+          GPRO: result,
+          SAP: rfcResult,
+        };
+      } catch (ex) {
+        draft.response.body = {
+          E_STATUS: "E",
+          E_MESSAGE: [ex.message, ex.description].filter(Boolean).join(" "),
+        };
+      }
+
+      break;
     default:
       draft.response.body = {
         E_STATUS: "E",
