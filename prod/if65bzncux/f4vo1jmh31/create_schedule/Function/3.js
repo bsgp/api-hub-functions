671978b6e91097449fd2c8cf5d3e0a89d386ea71
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-17 15:05:00 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_schedule

--- prod/if65bzncux/f4vo1jmh31/create_schedule/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/create_schedule/Function/3.js
@@ -2,7 +2,7 @@ module.exports = async (draft, { request, task }) => {
   // const never = "0 0 * * ? 9999";
 
   // 5am
-  const oneTime = "50 5 * * ? *";
+  const oneTime = "10 6 * * ? *";
 
   const seqNumList = [
     {
