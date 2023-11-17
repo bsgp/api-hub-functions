f6f0033cb7344642a667c2359aa0654f8fcdb508
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-17 09:37:41 +0900

    feat: migrate files to dev/dpznzimld2/3dbhzxqle8/0816test

--- dev/dpznzimld2/3dbhzxqle8/0816test/Function/5.js
+++ dev/dpznzimld2/3dbhzxqle8/0816test/Function/5.js
@@ -1,7 +1,12 @@
 module.exports = async (draft, context) => {
-  // your
-  return {
-    draft,
-    context,
+  const routeTo = {
+    exit: "Output#2",
   };
+  const setFailedResponse = (msg, statusCd = 400) => {
+    draft.response.body = { E_STATUS: "F", E_MESSAGE: msg };
+    draft.response.statusCode = statusCd;
+    draft.json.nextNodeKey = routeTo.exit;
+  };
+
+  return setFailedResponse(context);
 };
