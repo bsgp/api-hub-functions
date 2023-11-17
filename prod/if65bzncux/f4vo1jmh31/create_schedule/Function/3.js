4f041b03335349002c169d28b2a33f11ae14144b
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-17 16:25:17 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_schedule

--- prod/if65bzncux/f4vo1jmh31/create_schedule/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/create_schedule/Function/3.js
@@ -1,8 +1,20 @@
 module.exports = async (draft, { request, task }) => {
   // const never = "0 0 * * ? 9999";
 
-  // 5am
+  // now
   const oneTime = "30 6 * * ? *";
+
+  // first (11pm)
+  const firstTime = "0 14 * * ? *";
+
+  // second (11pm 10min)
+  const secondTime = "10 14 * * ? *";
+
+  // third (11pm 20min)
+  const thirdTime = "20 14 * * ? *";
+
+  // cron: "0 0/1 * * ? *",
+  // cron: "0 17 L,1-9 * ? *",
 
   const seqNumList = [
     {
@@ -11,11 +23,27 @@ module.exports = async (draft, { request, task }) => {
       Function: {
         Name: "ZCO_IF_HR_DEPT",
       },
-      cron: oneTime,
+      cron: firstTime,
       flowName: "if_gpro",
     },
-    // cron: "0 0/1 * * ? *",
-    // cron: "0 17 L,1-9 * ? *",
+    {
+      InterfaceId: "IF-CO-011-BATCH",
+      Data: {},
+      Function: {
+        Name: "ZCO_IF_HR_EMPLOYEE",
+      },
+      cron: secondTime,
+      flowName: "if_gpro",
+    },
+    {
+      InterfaceId: "IF-CO-035-BATCH",
+      Data: {},
+      Function: {
+        Name: "ZCO_IF_HR_EMPLOYEE_ORDER",
+      },
+      cron: thirdTime,
+      flowName: "if_gpro",
+    },
   ];
 
   for (let idx = 0; idx < seqNumList.length; idx += 1) {
