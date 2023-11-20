4ba097e973c95a6b0f5b0dcc4090380377238d6e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-20 22:07:03 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_gpro

--- prod/if65bzncux/f4vo1jmh31/if_gpro/Function/7.js
+++ prod/if65bzncux/f4vo1jmh31/if_gpro/Function/7.js
@@ -1,18 +1,66 @@
-module.exports = async (draft, { fn, restApi }) => {
+module.exports = async (draft, { request, tryit }) => {
   const { ifObj } = draft.json;
 
   switch (ifObj.InterfaceId) {
     case "IF-CT-003":
       try {
-        const token = await fn.getToken({ restApi });
-        const result = await fn.getOrganizationsList(token, {
-          restApi,
-        });
+        const reqItem = tryit(() => request.body.Data.draft) || {
+          workflows: [],
+        };
+        // request.body.Data.draft.documentNo "BSG-231120-0003"
+        // request.body.Data.draft.draftTemplateNo "BSGP-0005-1"
+        // request.body.Data.draft.draftStatusCode "DRF"
+        // request.body.Data.draft.draftId 280
+        // request.body.Data.draft.draftTemplateId 1114
+        // request.body.Data.draft.draftTemplateType "R"
+        // request.body.Data.draft.userId 2
+        // request.body.Data.draft.userName "임시관리자일"
+        // request.body.Data.draft.organizationId 26
+        // request.body.Data.draft.organizationName "(주)비에스지파트너스"
+        // request.body.Data.draft.workflows[].userId
+        // request.body.Data.draft.workflows[].userName
+        // request.body.Data.draft.workflows[].orderSeq
+        // request.body.Data.draft.workflows[].workflowTypeCode
+        // request.body.Data.draft.workflows[].organizationId
+        // request.body.Data.draft.workflows[].organizationName
+
+        const {
+          documentNo,
+          draftTemplateNo,
+          draftStatusCode,
+          draftId,
+          draftTemplateId,
+          draftTemplateType,
+          userId,
+          userName,
+          organizationId,
+          organizationName,
+        } = reqItem;
+        const workflows = reqItem.workflows.map((each) => ({
+          userId: each.userId,
+          userName: each.userName,
+          orderSeq: each.orderSeq,
+          workflowTypeCode: each.workflowTypeCode,
+          organizationId: each.organizationId,
+          organizationName: each.organizationName,
+        }));
 
         draft.response.body = {
           E_STATUS: "S",
           E_MESSAGE: "성공",
-          GPRO: result,
+          gpro: {
+            documentNo,
+            draftTemplateNo,
+            draftStatusCode,
+            draftId,
+            draftTemplateId,
+            draftTemplateType,
+            userId,
+            userName,
+            organizationId,
+            organizationName,
+            workflows,
+          },
         };
       } catch (ex) {
         draft.response.body = {
