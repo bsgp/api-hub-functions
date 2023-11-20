06ad2c3e00196555ea78e33ba376c5eca6a92762
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-20 17:25:52 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/9.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/9.js
@@ -1,67 +1,72 @@
 module.exports = async (draft, { sql, env, tryit, fn, dayjs }) => {
-  const { tables, newData } = draft.json;
+  const { interfaceID, tables, newData } = draft.json;
+  const sqlParams = { useCustomRole: false, stage: env.CURRENT_ALIAS };
   const { contractID } = newData;
 
-  const changedData = await sql("mysql", {
-    useCustomRole: false,
-    stage: env.CURRENT_ALIAS,
-  })
-    .select(tables["change"].name)
-    .where("row_key", "like", `${contractID}`)
-    .orWhere("row_key", "like", `${contractID}%`)
-    .orderBy("changed_at")
-    .run();
+  switch (interfaceID) {
+    case "IF-CT-109": {
+      const changedData = await sql("mysql", sqlParams)
+        .select(tables["change"].name)
+        .where("row_key", "like", `${contractID}`)
+        .orWhere("row_key", "like", `${contractID}%`)
+        .orderBy("changed_at")
+        .run();
 
-  const chagedList = tryit(() => changedData.body.list, []);
+      const chagedList = tryit(() => changedData.body.list, []);
 
-  draft.response.body = {
-    E_MESSAGE: "변경내역 조회가 완료되었습니다",
-    E_STATUS: "S",
-    contractID,
-    chagedList,
-    history: {
-      contract: chagedList
-        .filter((list) => list.type === "contract")
-        .map(({ changed_at, changed_by, content }) => ({
-          changed_at: fn.convDate(dayjs, changed_at),
-          changed_by,
-          content: JSON.stringify(content),
-        })),
-      partyList: chagedList
-        .filter((list) => list.type === "party")
-        .map(({ changed_at, changed_by, content }) => ({
-          changed_at: fn.convDate(dayjs, changed_at),
-          changed_by,
-          content: JSON.stringify(content),
-        })),
-      costObjectList: chagedList
-        .filter((list) => list.type === "cost_object")
-        .map(({ changed_at, changed_by, content }) => ({
-          changed_at: fn.convDate(dayjs, changed_at),
-          changed_by,
-          content: JSON.stringify(content),
-        })),
-      wbsList: chagedList
-        .filter((list) => list.type === "wbs")
-        .map(({ changed_at, changed_by, content }) => ({
-          changed_at: fn.convDate(dayjs, changed_at),
-          changed_by,
-          content: JSON.stringify(content),
-        })),
-      billList: chagedList
-        .filter((list) => list.type === "bill")
-        .map(({ changed_at, changed_by, content }) => ({
-          changed_at: fn.convDate(dayjs, changed_at),
-          changed_by,
-          content: JSON.stringify(content),
-        })),
-      attachmentList: chagedList
-        .filter((list) => list.type === "attachment")
-        .map(({ changed_at, changed_by, content }) => ({
-          changed_at: fn.convDate(dayjs, changed_at),
-          changed_by,
-          content: JSON.stringify(content),
-        })),
-    },
-  };
+      draft.response.body = {
+        E_MESSAGE: "변경내역 조회가 완료되었습니다",
+        E_STATUS: "S",
+        contractID,
+        chagedList,
+        history: {
+          contract: chagedList
+            .filter((list) => list.type === "contract")
+            .map(({ changed_at, changed_by, content }) => ({
+              changed_at: fn.convDate(dayjs, changed_at),
+              changed_by,
+              content: JSON.stringify(content),
+            })),
+          partyList: chagedList
+            .filter((list) => list.type === "party")
+            .map(({ changed_at, changed_by, content }) => ({
+              changed_at: fn.convDate(dayjs, changed_at),
+              changed_by,
+              content: JSON.stringify(content),
+            })),
+          costObjectList: chagedList
+            .filter((list) => list.type === "cost_object")
+            .map(({ changed_at, changed_by, content }) => ({
+              changed_at: fn.convDate(dayjs, changed_at),
+              changed_by,
+              content: JSON.stringify(content),
+            })),
+          wbsList: chagedList
+            .filter((list) => list.type === "wbs")
+            .map(({ changed_at, changed_by, content }) => ({
+              changed_at: fn.convDate(dayjs, changed_at),
+              changed_by,
+              content: JSON.stringify(content),
+            })),
+          billList: chagedList
+            .filter((list) => list.type === "bill")
+            .map(({ changed_at, changed_by, content }) => ({
+              changed_at: fn.convDate(dayjs, changed_at),
+              changed_by,
+              content: JSON.stringify(content),
+            })),
+          attachmentList: chagedList
+            .filter((list) => list.type === "attachment")
+            .map(({ changed_at, changed_by, content }) => ({
+              changed_at: fn.convDate(dayjs, changed_at),
+              changed_by,
+              content: JSON.stringify(content),
+            })),
+        },
+      };
+      break;
+    }
+    default:
+      break;
+  }
 };
