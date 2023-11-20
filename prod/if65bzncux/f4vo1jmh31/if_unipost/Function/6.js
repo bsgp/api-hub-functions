2351870318c2861554e4b5807ba5668c2dcb02a9
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-20 17:41:16 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_unipost

--- prod/if65bzncux/f4vo1jmh31/if_unipost/Function/6.js
+++ prod/if65bzncux/f4vo1jmh31/if_unipost/Function/6.js
@@ -69,7 +69,9 @@ module.exports = async (draft, { request, clone, tryit, file, env, sql }) => {
     .run();
 
   /**
-   * 서명완료 시 changed_contract: 차수, 수정내역 업데이트
+   * webhook으로 data를 받을 때
+   * changed_contract: seq가 있으면 차수, 수정내역 업데이트
+   * changed_contract: seq가 없으면 차수, 계약변경 내역 업데이트
    */
 
   draft.response.body = {
