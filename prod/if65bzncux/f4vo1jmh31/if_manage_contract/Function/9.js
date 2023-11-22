6c1a87745e99a1c081e1984d6edc2be5fc0443d3
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-22 14:23:39 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/9.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/9.js
@@ -122,7 +122,7 @@ module.exports = async (draft, { sql, env, tryit, fn, dayjs }) => {
             .where("seq", "like", form.seq)
             .run();
           const latestData = tryit(() => getLatestData.body.list[0], {});
-          const latestJsonData = latestData.after;
+          const latestJsonData = latestData && latestData.after;
           if (!latestJsonData) {
             draft.response.body = {
               E_MESSAGE: "이전 차수 계약정보가 없습니다",
