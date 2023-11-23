2d2d5530ae5b1b75aea357d347889a5ad4c76ceb
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-23 13:29:44 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/functions.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/functions.js
@@ -129,6 +129,7 @@ module.exports.cost_object =
     table.string("post_date", 8).defaultTo(""); // 청구예정일
     table.string("loekz", 1).defaultTo(""); // 삭제지시자(구매)
     table.string("remark", 100).defaultTo(""); // 비고
+    table.boolean("extra_item").defaultTo(false); // 청구계획 추가항목
     table.boolean("deleted").defaultTo(false);
 
     table.primary(["contract_id", "id"]);
