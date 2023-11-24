ce07834974b6869c8662a8f0e569e3cb6aa4fd2e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-24 15:46:42 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/upload_contract_status_map

---
+++ prod/if65bzncux/f4vo1jmh31/upload_contract_status_map/Function/3.js
@@ -0,0 +1,102 @@
+module.exports = async (draft, { file, env }) => {
+  const statusMap = {
+    RQN: {
+      name: "사업부 기안 상신",
+      next: "APN",
+      editable: false,
+    },
+    APN: {
+      name: "사업부 기안 승인완료",
+      next: "DRN",
+      editable: false,
+    },
+    DRN: {
+      name: "신규계약작성중",
+      next: "LRN",
+      editable: true,
+    },
+    LRN: {
+      name: "FnA 내부기안 상신",
+      next: "LRC",
+      editable: false,
+    },
+    LRR: {
+      name: "FnA 내부기안 반려",
+      next: "MOD",
+      editable: true,
+    },
+    LRC: {
+      name: "FnA 내부기안 승인완료",
+      next: ["END", "ENC", "ECC", "TCC", "DRC"],
+      editable: false,
+    },
+    ENC: {
+      name: "전자(신규)계약 생성완료",
+      next: "RSC",
+      editable: false,
+    },
+    RSC: {
+      name: "서명요청발송 완료",
+      next: ["OSR"],
+      editable: false,
+    },
+    OSC: {
+      name: "상대방 서명 완료",
+      next: "SSC",
+      editable: false,
+    },
+    SSC: {
+      name: "서명 완료",
+      next: "END",
+      editable: false,
+    },
+    OSR: {
+      name: "상대방 서명 거부(반려)",
+      next: "MOD",
+      editable: true,
+    },
+    ECC: {
+      name: "전자(변경)계약 생성완료",
+      next: "RSC",
+      editable: false,
+    },
+    TCC: {
+      name: "계약해지를 위한 전자(변경)계약 생성완료",
+      next: "RSC",
+      editable: false,
+    },
+    TRM: {
+      name: "폐기됨",
+      next: "LRN",
+      editable: false,
+    },
+    DRC: {
+      name: "변경계약작성중",
+      next: "LRN",
+      editable: true,
+    },
+    MOD: {
+      name: "수정중",
+      next: "LRN",
+      editable: true,
+    },
+    CNL: {
+      name: "해지됨",
+      editable: false,
+    },
+  };
+
+  await file.upload("config/contract_status.json", statusMap, {
+    gzip: true,
+    stage: env.CURRENT_ALIAS,
+  });
+  const url = await file.getUrl("config/contract_status.json", {
+    stage: env.CURRENT_ALIAS,
+  });
+
+  draft.response.body = {
+    url,
+    count: Object.keys(statusMap).length,
+    list: statusMap,
+  };
+};
