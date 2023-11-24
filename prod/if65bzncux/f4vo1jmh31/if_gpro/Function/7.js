4cd50504bce9641176326143426ef0bad6cf37ca
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-24 15:15:02 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_gpro

--- prod/if65bzncux/f4vo1jmh31/if_gpro/Function/7.js
+++ prod/if65bzncux/f4vo1jmh31/if_gpro/Function/7.js
@@ -1,26 +1,46 @@
-module.exports = async (draft, { request, tryit }) => {
-  const { ifObj } = draft.json;
+// request.body.Data.draft.documentNo "BSG-231120-0003"
+// request.body.Data.draft.draftTemplateNo "BSGP-0005-1"
+// request.body.Data.draft.draftStatusCode "DRF"
+// request.body.Data.draft.draftId 280
+// request.body.Data.draft.draftTemplateId 1114
+// request.body.Data.draft.draftTemplateType "R"
+// request.body.Data.draft.userId 2
+// request.body.Data.draft.userName "임시관리자일"
+// request.body.Data.draft.organizationId 26
+// request.body.Data.draft.organizationName "(주)비에스지파트너스"
+// request.body.Data.draft.workflows[].userId
+// request.body.Data.draft.workflows[].userName
+// request.body.Data.draft.workflows[].orderSeq
+// request.body.Data.draft.workflows[].workflowTypeCode
+// request.body.Data.draft.workflows[].organizationId
+// request.body.Data.draft.workflows[].organizationName
 
+module.exports = async (draft, { request, tryit, file, sql }) => {
+  const { ifObj } = draft.json;
+  const { stage } = request;
+
+  // const statusMap = {
+  //   ""
+  // }
+
+  const tables = await file.get("config/tables.json", {
+    gziped: true,
+    toJSON: true,
+    stage,
+  });
+
+  const query = sql("mysql", { useCustomRole: false, stage });
+
+  // draft.response.body = {
+  //   webhookData,
+  //   fStatus,
+  //   updateResult,
+  //   E_STATUS: updateResult.statusCode === "200" ? "S" : "F",
+  // };
   switch (ifObj.InterfaceId) {
     case "IF-CT-003":
       try {
         const reqItem = tryit(() => request.body.Data.draft);
-        // request.body.Data.draft.documentNo "BSG-231120-0003"
-        // request.body.Data.draft.draftTemplateNo "BSGP-0005-1"
-        // request.body.Data.draft.draftStatusCode "DRF"
-        // request.body.Data.draft.draftId 280
-        // request.body.Data.draft.draftTemplateId 1114
-        // request.body.Data.draft.draftTemplateType "R"
-        // request.body.Data.draft.userId 2
-        // request.body.Data.draft.userName "임시관리자일"
-        // request.body.Data.draft.organizationId 26
-        // request.body.Data.draft.organizationName "(주)비에스지파트너스"
-        // request.body.Data.draft.workflows[].userId
-        // request.body.Data.draft.workflows[].userName
-        // request.body.Data.draft.workflows[].orderSeq
-        // request.body.Data.draft.workflows[].workflowTypeCode
-        // request.body.Data.draft.workflows[].organizationId
-        // request.body.Data.draft.workflows[].organizationName
 
         if (!reqItem) {
           draft.response.body = {
@@ -51,9 +71,21 @@ module.exports = async (draft, { request, tryit }) => {
           organizationName: each.organizationName,
         }));
 
+        const draftContent = JSON.parse(reqItem.draftContent);
+        const { contractId } = draftContent.values;
+
+        query
+          .update(tables.contract.name, {
+            status: "",
+          })
+          .where({ id: contractId });
+
+        const updateResult = await query.run();
+
         draft.response.body = {
           E_STATUS: "S",
           E_MESSAGE: "성공",
+          updateResult,
           gpro: {
             documentNo,
             draftTemplateNo,
