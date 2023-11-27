31bd2479908febd1419993fc1910502525033142
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-11-27 15:50:33 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -19,30 +19,21 @@ module.exports = async (draft, { fn, sql, env, makeid }) => {
       const result = await mysql.table
         .create(spec.name, fn[tableKey]({ mysql, makeid }))
         .run();
-      if (result.statusCode !== 200) {
-        if (spec.desc === "groupware letter approval info DB") {
-          const alterResult = await mysql.table
-            .alter(spec.name, function (table) {
-              // table.string("apr_status", 3).defaultTo("");
-              table.string("gpro_document_no").defaultTo("");
-              table.string("gpro_draft_template_no").defaultTo("");
-              table.string("gpro_draft_status_code").defaultTo("");
-              table.string("gpro_draft_id").defaultTo("");
-              table.string("gpro_draft_templateId").defaultTo("");
-              table.string("gpro_draftTemplateType").defaultTo("");
-              table.string("gpro_userId").defaultTo("");
-              table.string("gpro_userName").defaultTo("");
-              table.string("gpro_organizationId").defaultTo("");
-              table.string("gpro_organizationName").defaultTo("");
-              table.json("gpro_workflows");
-              // table.boolean("extra_item").defaultTo(false);
-            })
-            .run();
-          draft.response.body[spec.name] = alterResult;
-        }
-      } else
-        draft.response.body[spec.name] =
-          result.statusCode === 200 ? "Succeed" : result.body;
+      // if (result.statusCode !== 200) {
+      //   if (spec.desc === "groupware letter approval info DB") {
+      //     const alterResult = await mysql.table
+      //       .alter(spec.name, function (table) {
+      //         table.string("gpro_organizationName").defaultTo("");
+      //         table.json("gpro_workflows");
+      //         // table.string("apr_status", 3).defaultTo("");
+      //         // table.boolean("extra_item").defaultTo(false);
+      //       })
+      //       .run();
+      //     draft.response.body[spec.name] = alterResult;
+      //   }
+      // } else
+      draft.response.body[spec.name] =
+        result.statusCode === 200 ? "Succeed" : result.body;
       return true;
     })
   );
