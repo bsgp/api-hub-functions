860d1c74ebfdd4182593d01c435e97868cd63816
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-12-08 10:09:42 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_gpro

--- prod/if65bzncux/f4vo1jmh31/if_gpro/Function/7.js
+++ prod/if65bzncux/f4vo1jmh31/if_gpro/Function/7.js
@@ -50,9 +50,12 @@ module.exports = async (draft, { request, tryit, file, sql, env, flow }) => {
     case "IF-CT-003":
       try {
         const reqItem = tryit(() => request.body.Data.draft);
+
         if (!reqItem) {
-          const E_MESSAGE = "request.body.Data.draft에 데이터가 없습니다";
-          draft.response.body = { E_STATUS: "E", E_MESSAGE };
+          draft.response.body = {
+            E_STATUS: "E",
+            E_MESSAGE: "request.body.Data.draft에 데이터가 없습니다",
+          };
           return;
         }
 
@@ -92,119 +95,85 @@ module.exports = async (draft, { request, tryit, file, sql, env, flow }) => {
             updateData.status = statusMap[statusFromDraftContent].next;
           }
         }
-        switch (draftTemplateNo) {
-          case "BSGP-0005-1": {
-            /**
-             * 기안서 (기존 계약문서, 외주계약 신규 요청)
-             * 기존 계약의 경우 해당 계약에 기안 상태 추가.
-             * 신규: 계약번호를 생성하고 contract테이블에 Insert.
-             */
-            let contractID;
-            if (!contractId) {
-              const flowPayload = {
-                id: "if_manage_contract",
-                body: {
-                  Data: {
-                    form: {
-                      contractID: "",
-                      name: draftTemplateName || "",
-                      curr_key: "KRW",
-                      curr_local: "KRW",
-                      type: "P",
-                      status: "APN",
-                    },
-                  },
-                  Function: {
-                    UserId: "g_pro",
-                    UserText: "g_pro webhook",
-                    Name: "POST_DATA_TO_DB",
-                    SysId: "SUPPORT",
-                    Type: "DB",
-                  },
-                  InterfaceId: "IF-CT-102",
+
+        let contractID;
+        if (!contractId) {
+          const flowPayload = {
+            id: "if_manage_contract",
+            body: {
+              Data: {
+                form: {
+                  contractID: "",
+                  name: draftTemplateName || "",
+                  curr_key: "KRW",
+                  curr_local: "KRW",
+                  type: "P",
+                  status: "APN",
                 },
-              };
-
-              const getContractID = await flow.run(flowPayload);
-              contractID = getContractID.contractID;
-            } else contractID = contractId;
-
-            /** contract db update */
-            const updateResult = await sql("mysql", sqlParams)
-              .update(tables.contract.name, updateData)
-              .where({ id: contractID })
-              .run();
-            /** letter_appr db update */
-            const updateApprDBResult = await sql("mysql", sqlParams)
-              .insert(tables["letter_appr"].name, {
-                contract_id: contractID,
-                id: documentNo,
-                gpro_document_no: documentNo,
-                gpro_draft_template_no: draftTemplateNo,
-                gpro_draft_template_name: draftTemplateName,
-                gpro_draft_status_code: draftStatusCode,
-                gpro_draft_id: draftId,
-                gpro_draft_templateId: draftTemplateId,
-                gpro_draftTemplateType: draftTemplateType,
-                gpro_userId: userId,
-                gpro_userName: userName,
-                gpro_organizationId: organizationId,
-                gpro_organizationName: organizationName,
-                gpro_workflows: JSON.stringify(workflows || []),
-              })
-              .onConflict()
-              .merge()
-              .run();
-
-            draft.response.body = {
-              E_STATUS: "S",
-              E_MESSAGE: ["성공"].join(" "),
-              contractID,
-              updateResult,
-              updateApprDBResult,
-              gpro: {
-                documentNo,
-                draftTemplateNo,
-                draftTemplateName,
-                draftStatusCode,
-                draftId,
-                draftTemplateId,
-                draftTemplateType,
-                userId,
-                userName,
-                organizationId,
-                organizationName,
-                workflows,
               },
-            };
-            break;
-          }
-          default: {
-            /**
-             * BSGP-0005-2 (외주계약 변경/파기 요청) 등
-             * unmap_letters테이블로 Insert
-             */
-            draft.response.body = {
-              E_STATUS: "S",
-              E_MESSAGE: ["성공", draftTemplateNo].join(" "),
-              gpro: {
-                documentNo,
-                draftTemplateNo,
-                draftTemplateName,
-                draftStatusCode,
-                draftId,
-                draftTemplateId,
-                draftTemplateType,
-                userId,
-                userName,
-                organizationId,
-                organizationName,
-                workflows,
+              Function: {
+                UserId: "g_pro",
+                UserText: "g_pro webhook",
+                Name: "POST_DATA_TO_DB",
+                SysId: "SUPPORT",
+                Type: "DB",
               },
-            };
-            break;
-          }
-        }
+              InterfaceId: "IF-CT-102",
+            },
+          };
+
+          const getContractID = await flow.run(flowPayload);
+          contractID = getContractID.contractID;
+        } else contractID = contractId;
+
+        /** contract db update */
+        const updateResult = await sql("mysql", sqlParams)
+          .update(tables.contract.name, updateData)
+          .where({ id: contractID })
+          .run();
+        /** letter_appr db update */
+        const updateApprDBResult = await sql("mysql", sqlParams)
+          .insert(tables.letter_appr.name, {
+            contract_id: contractID,
+            id: documentNo,
+            gpro_document_no: documentNo,
+            gpro_draft_template_no: draftTemplateNo,
+            gpro_draft_template_name: draftTemplateName,
+            gpro_draft_status_code: draftStatusCode,
+            gpro_draft_id: draftId,
+            gpro_draft_templateId: draftTemplateId,
+            gpro_draftTemplateType: draftTemplateType,
+            gpro_userId: userId,
+            gpro_userName: userName,
+            gpro_organizationId: organizationId,
+            gpro_organizationName: organizationName,
+            gpro_workflows: JSON.stringify(workflows || []),
+          })
+          .onConflict()
+          .merge()
+          .run();
+
+        draft.response.body = {
+          E_STATUS: "S",
+          E_MESSAGE: ["성공"].join(" "),
+          contractID,
+          updateResult,
+          updateApprDBResult,
+          gpro: {
+            documentNo,
+            draftTemplateNo,
+            draftTemplateName,
+            draftStatusCode,
+            draftId,
+            draftTemplateId,
+            draftTemplateType,
+            userId,
+            userName,
+            organizationId,
+            organizationName,
+            workflows,
+          },
+        };
       } catch (ex) {
         draft.response.body = {
           E_STATUS: "E",
