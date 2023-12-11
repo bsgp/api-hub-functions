416546551977011b944ae388ef8f36c66564045e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-12-11 10:57:32 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/11.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/11.js
@@ -26,6 +26,16 @@ module.exports = async (draft, { sql, env }) => {
     case "IF-CT-119": {
       // MAPPING_LETTER_AND_CONTRACT
       const { source, target } = newData;
+      const contract_id = target.id;
+      if (!contract_id) {
+        draft.response.body = {
+          E_STATUS: "F",
+          E_MESSAGE: "요청이 잘못되었습니다",
+          source,
+          target,
+        };
+        return;
+      }
       draft.response.body = {
         E_STATUS: "S",
         E_MESSAGE: "준비중",
