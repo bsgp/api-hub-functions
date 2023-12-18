4477ea3387c85e050721dcf7e9c6421215c76eb4
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-12-18 11:23:06 +0900

    feat: migrate files to prod/7na1k6mtgy/wk79tmdlvu/get_pdf_template_string

---
+++ prod/7na1k6mtgy/wk79tmdlvu/get_pdf_template_string/Function/3.js
@@ -0,0 +1,38 @@
+module.exports = async (draft, { request, file }) => {
+  const { path } = request.body;
+
+  let response_body = {
+    E_STATUS: "S",
+    E_MSG: "Saved",
+    E_DATA: {},
+    E_PATH: { path: path },
+  };
+
+  switch (request.method) {
+    case "GET": {
+      try {
+        const templateJSON = await file.get(path, { toJSON: true });
+
+        response_body = {
+          E_STATUS: "S",
+          E_MSG: "GET IS DONE",
+          E_DATA: { templateJSON: templateJSON },
+          E_PATH: { path: path },
+        };
+      } catch (err) {
+        response_body = {
+          E_STATUS: "F",
+          E_MSG: "GET IS FAILED",
+          E_DATA: { err },
+        };
+      }
+      break;
+    }
+
+    default: {
+      response_body = "Default value is returned";
+    }
+  }
+
+  draft.response.body = response_body;
+};
