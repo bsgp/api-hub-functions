d218bdcb778f47a4a2aadab0c457623122f9c77e
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-12-18 15:48:18 +0900

    feat: migrate files to prod/7na1k6mtgy/1srrod8gih/lc_ui5

--- prod/7na1k6mtgy/1srrod8gih/lc_ui5/functions.js
+++ prod/7na1k6mtgy/1srrod8gih/lc_ui5/functions.js
@@ -265,11 +265,11 @@ const doUpdatePath = async (data, { dynamodb, tableName, isFalsy }) => {
       }
     }
   } else {
-    const pathRegExp = /(?<=:)[\w-]+/g;
-    let index = 0;
+    // const pathRegExp = /(?<=:)[\w-]+/g;
+    // let index = 0;
     let dataOldPath;
 
-    const dynamicPath = path.replace(pathRegExp, () => `${index++}`);
+    // const dynamicPath = path.replace(pathRegExp, () => `${index++}`);
 
     if (oldPath) {
       dataOldPath = await dynamodb.getItem(
@@ -297,11 +297,11 @@ const doUpdatePath = async (data, { dynamodb, tableName, isFalsy }) => {
           { useCustomerRole: false }
         );
 
-        await dynamodb.deleteItem(
-          tableName,
-          { pkid: "pattern", skid: dynamicPath },
-          { useCustomerRole: false }
-        );
+        // await dynamodb.deleteItem(
+        //   tableName,
+        //   { pkid: "pattern", skid: dynamicPath },
+        //   { useCustomerRole: false }
+        // );
       } else {
         throw new Error(["Old path", oldPath, "does not exist"].join(" "));
       }
@@ -323,22 +323,22 @@ const doUpdatePath = async (data, { dynamodb, tableName, isFalsy }) => {
       }
     );
 
-    const params = path.match(pathRegExp);
+    // const params = path.match(pathRegExp);
 
-    const newPattern = {
-      metaId: id,
-      params: params || [],
-      length: path.split("/").length - 1,
-    };
+    // const newPattern = {
+    //   metaId: id,
+    //   params: params || [],
+    //   length: path.split("/").length - 1,
+    // };
 
-    await dynamodb.insertItem(
-      tableName,
-      { pkid: "pattern", skid: dynamicPath },
-      newPattern,
-      {
-        useCustomerRole: false,
-      }
-    );
+    // await dynamodb.insertItem(
+    //   tableName,
+    //   { pkid: "pattern", skid: dynamicPath },
+    //   newPattern,
+    //   {
+    //     useCustomerRole: false,
+    //   }
+    // );
 
     await dynamodb.updateItem(
       tableName,
