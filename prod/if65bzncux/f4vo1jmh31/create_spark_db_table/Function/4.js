3671f70774cd5c09f6a4b05d58b589faf4e46ecd
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-12-22 11:56:51 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/create_spark_db_table

--- prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
+++ prod/if65bzncux/f4vo1jmh31/create_spark_db_table/Function/4.js
@@ -20,7 +20,7 @@ module.exports = async (draft, { fn, sql, env, makeid }) => {
         .create(spec.name, fn[tableKey]({ mysql, makeid }))
         .run();
       if (result.statusCode !== 200) {
-        if (spec.desc === "Contract info DB table") {
+        if (spec.desc === "Actual FI cost object DB table") {
           const alterResult = await mysql.table
             .alter(spec.name, function (table) {
               table.string("origin_item_index", 5).defaultTo("");
