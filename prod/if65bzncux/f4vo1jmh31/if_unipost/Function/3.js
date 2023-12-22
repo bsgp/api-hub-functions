5ac32d523503cab0aa8a419e4139a186e8f50eaa
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-12-22 15:21:35 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_unipost

--- prod/if65bzncux/f4vo1jmh31/if_unipost/Function/3.js
+++ prod/if65bzncux/f4vo1jmh31/if_unipost/Function/3.js
@@ -11,49 +11,32 @@ module.exports = async (draft, { request, env, restApi, fn }) => {
     stage === "prd"
       ? "https://cont.unipost.co.kr"
       : "https://contdev.unipost.co.kr";
-  const secretKey = await fn.getSecretKey({ restApi, unipostURL });
+  const opt = { restApi, unipostURL };
+  const secretKey = await fn.getSecretKey(opt);
 
   switch (draft.json.ifObj.InterfaceId) {
-    case "IF-CT-007":
-      {
-        const result = await fn.getTemplateList(secretKey, {
-          restApi,
-          unipostURL,
-        });
-        draft.response.body = {
-          E_STATUS: "S",
-          list: result,
-        };
-      }
+    case "IF-CT-007": {
+      const result = await fn.getTemplateList(secretKey, opt);
+      draft.response.body = {
+        E_STATUS: "S",
+        list: result,
+      };
       break;
-    case "IF-CT-008":
-      {
-        const token = await fn.getTokenForWork(secretKey, {
-          restApi,
-          unipostURL,
-          contNo: request.body.Data.ContNo,
-        });
-        draft.response.body = {
-          E_STATUS: "S",
-          unipostURL,
-          token,
-        };
-      }
+    }
+    case "IF-CT-008": {
+      const contNo = request.body.Data.ContNo;
+      const token = await fn.getTokenForWork(secretKey, { ...opt, contNo });
+
+      draft.response.body = { E_STATUS: "S", unipostURL, token };
       break;
-    case "IF-CT-009":
-      {
-        const token = await fn.getTokenForRead(secretKey, {
-          restApi,
-          unipostURL,
-          contNo: request.body.Data.ContNo,
-        });
-        draft.response.body = {
-          E_STATUS: "S",
-          unipostURL,
-          token,
-        };
-      }
+    }
+    case "IF-CT-009": {
+      const contNo = request.body.Data.ContNo;
+      const token = await fn.getTokenForRead(secretKey, { ...opt, contNo });
+
+      draft.response.body = { E_STATUS: "S", unipostURL, token };
       break;
+    }
     default:
       break;
   }
