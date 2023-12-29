3f71ea81e51843fc8c5ccaeef0794d1f2f4b61d1
GoonNahm Seo <kieat.seo.kr@gmail.com>
2023-12-29 10:43:19 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
@@ -11,14 +11,14 @@ module.exports = async (draft, { sql, env, tryit, fn, dayjs, user }) => {
           .select(`contract.*`)
           .leftJoin(
             sql("mysql", sqlParams)
-              .select(`${tables.party.name} as party`)
               .select(
                 `contract_id`,
                 `ref_id`,
                 `stems10`,
                 `name as party_name`,
                 `deleted as party_deleted`
-              ),
+              )
+              .from(`${tables.party.name}`),
             function () {
               this.on(function () {
                 this.on(`contract_id`, `contract.id`);
