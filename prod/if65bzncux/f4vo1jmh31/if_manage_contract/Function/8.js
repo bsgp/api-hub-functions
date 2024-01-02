709a66a67b2f7e26207f1830c29e242305272045
GoonNahm Seo <kieat.seo.kr@gmail.com>
2024-01-02 10:14:10 +0900

    feat: migrate files to prod/if65bzncux/f4vo1jmh31/if_manage_contract

--- prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
+++ prod/if65bzncux/f4vo1jmh31/if_manage_contract/Function/8.js
@@ -19,7 +19,8 @@ module.exports = async (draft, { sql, env, tryit, fn, dayjs, user }) => {
           .leftJoin(`${tables.party.name} as party`, function () {
             this.on(`party.contract_id`, `contract.id`)
               .onNotIn("party.ref_id", ["1000", "KR01", "US01"])
-              .onIn("party.index", ["1", "2"]);
+              .onIn("party.index", ["1", "2"])
+              .onNotIn("party.deleted", [true]);
           });
         // .where(function () {
         //   this.where(function () {
