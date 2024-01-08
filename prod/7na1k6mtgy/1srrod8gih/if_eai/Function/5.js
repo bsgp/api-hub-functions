412eab7c0bedf909ad2e72aa15b1bf3f74e57583
GoonNahm Seo <kieat.seo.kr@gmail.com>
2024-01-08 13:05:50 +0900

    feat: migrate files to prod/7na1k6mtgy/1srrod8gih/if_eai

---
+++ prod/7na1k6mtgy/1srrod8gih/if_eai/Function/5.js
@@ -0,0 +1,13 @@
+module.exports = async (draft, { request }) => {
+  switch (request.method) {
+    case "GET":
+      // code
+      break;
+    case "POST":
+      break;
+    case "PUT":
+      break;
+    default:
+    // code
+  }
+};
