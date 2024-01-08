8043c2c295d226b01c8da32783946daa7d257374
GoonNahm Seo <kieat.seo.kr@gmail.com>
2024-01-08 13:23:12 +0900

    feat: migrate files to prod/7na1k6mtgy/1srrod8gih/if_eai

--- prod/7na1k6mtgy/1srrod8gih/if_eai/Function/5.js
+++ prod/7na1k6mtgy/1srrod8gih/if_eai/Function/5.js
@@ -1,9 +1,21 @@
-module.exports = async (draft, { request }) => {
+module.exports = async (draft, { request, dynamodb, fn }) => {
+  const tableName = "eai_dev";
+
   switch (request.method) {
     case "GET":
-      // code
+      {
+        const result = await fn.getAllConfigs({ dynamodb, tableName });
+        draft.response.body = result;
+      }
       break;
     case "POST":
+      {
+        const result = await fn.saveConfig(request.body, {
+          dynamodb,
+          tableName,
+        });
+        draft.response.body = result;
+      }
       break;
     case "PUT":
       break;
